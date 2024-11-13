6667b25f3b0861a14f5acc0fec03089bd298a57e
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2024-11-13 16:38:28 -0600

    Update file 11/13/2024, 4:38:29 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,13 +1,15 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return true;
             }
+            set.add(num);
         }
-        
-        return false;        
+        return false;
     }
 }
