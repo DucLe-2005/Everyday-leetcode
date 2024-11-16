6f94ec7e293388c267e13482a3a9498a316a53df
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2024-11-16 13:59:48 -0600

    Update file 11/16/2024, 1:59:49 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,32 +1,30 @@
-import java.util.HashMap;
-import java.util.ArrayList;
-
 class Solution {
     public boolean isValid(String s) {
-        char[] sArray = s.toCharArray();
-        List<Character> stack = new ArrayList<>();
-        HashMap<Character, Character> closeToOpen = new HashMap<>();
-        closeToOpen.put(')', '(');
-        closeToOpen.put('}', '{');
-        closeToOpen.put(']', '[');        
-
-        for (char c : sArray) {
-            if (closeToOpen.containsKey(c)) {
-                if (stack.size() != 0 && stack.get(stack.size() - 1) == closeToOpen.get(c)) {
-                    stack.remove(stack.size() - 1);
-                }
-                else {
+        // Create an empty stack to keep track of opening brackets
+        Stack<Character> stack = new Stack<Character>();
+        
+        // Loop through every character in the string
+        for (char c : s.toCharArray()) {
+            // If the character is an opening bracket, push it onto the stack
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else { // If the character is a closing bracket
+                // If the stack is empty, there is no matching opening bracket, so return false
+                if (stack.isEmpty()) {
                     return false;
                 }
-            } else {
-                stack.add(c);
+                // Otherwise, get the top of the stack and check if it's the matching opening bracket
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    // If it is, pop the opening bracket from the stack
+                    stack.pop();
+                } else { // Otherwise, the brackets don't match, so return false
+                    return false;
+                }
             }
         }
-
-        if (stack.size() != 0) {
-            return false;
-        }
-
-        return true;
+        // If the stack is empty, all opening brackets have been closed, so return true
+        // Otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
     }
 }
