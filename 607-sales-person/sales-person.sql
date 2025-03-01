91cc3b19af0bd2888cb2add2d8c845d7be897019
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2025-03-01 15:48:55 -0600

    Time: 3019 ms (25.71%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -5,6 +5,9 @@ WHERE sales_id NOT IN
     (
         SELECT o.sales_id
         FROM Orders o
-        LEFT JOIN company c ON c.com_id = o.com_id
-        WHERE c.name = 'RED'
+        WHERE o.com_id IN (
+            SELECT com_id
+            FROM COMPANY c
+            WHERE c.name = 'RED'
+        )
     );
