1902e55e4ead6a3b683de952258c16a2d58db759
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2025-03-02 16:57:28 -0600

    Time: 1240 ms (26.09%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,11 +1,11 @@
-SELECT e.employee_id 
-FROM employees e
+SELECT employee_id 
+FROM employees 
 WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
 
 UNION
 
-SELECT s.employee_id
-FROM salaries s
+SELECT employee_id
+FROM salaries 
 WHERE employee_id NOT IN (SELECT employee_id FROM employees)
 
 ORDER BY employee_id
