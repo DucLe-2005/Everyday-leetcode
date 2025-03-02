4ea0cf006920d92a9463701c7d44b7586c79614c
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2025-03-02 16:56:50 -0600

    Time: 1296 ms (21.33%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,9 +1,11 @@
-SELECT F.employee_id
-FROM (
-    SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
-    UNION
-    SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id)
-)
-AS F
-WHERE F.salary IS NULL OR F.name IS NULL
-ORDER BY F.employee_id;
+SELECT e.employee_id 
+FROM employees e
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+
+UNION
+
+SELECT s.employee_id
+FROM salaries s
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+
+ORDER BY employee_id
