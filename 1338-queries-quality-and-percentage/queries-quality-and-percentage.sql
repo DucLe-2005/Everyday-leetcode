914576e4e0a44cc9561e33a8f680ca8b791c7058
DucLe-2005 <135077375+DucLe-2005@users.noreply.github.com>
2025-03-07 14:06:10 -0600

    Time: 759 ms (34.17%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,7 +1,7 @@
 SELECT 
     query_name, 
     ROUND(avg(rating/position) ,2) AS quality, 
-    ROUND((sum(case when rating < 3 then 1 else 0 end)*100/count(rating)),2) as poor_query_percentage
+    ROUND(AVG(case when rating < 3 then 1 else 0 end) *100, 2) as poor_query_percentage
 FROM
     Queries
 GROUP BY
