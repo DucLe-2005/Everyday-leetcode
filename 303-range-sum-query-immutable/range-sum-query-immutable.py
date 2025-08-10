4602fb8861052862d22fe21e6bb4876be0f8501c
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-08-10 01:14:31 -0500

    Time: 0 ms (100.00%) | Memory: 21.8 MB (26.82%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -0,0 +1,13 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        cur = 0
+        for n in nums:
+            cur += n
+            self.prefix.append(cur)
+
+    def sumRange(self, left: int, right: int) -> int:
+        rightSum = self.prefix[right]
+        leftSum = self.prefix[left - 1] if left - 1 >= 0 else 0
+        return rightSum - leftSum
