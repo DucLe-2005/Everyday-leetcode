5da17ef8980830bfc13ea70d3dc0eff627381e56
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-08-15 09:44:24 -0500

    Time: 161 ms (32.07%) | Memory: 78 MB (78.48%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,46 +1,43 @@
 class Node:
-    def __init__(self, key: int, val: int):
-        self.val = val
+    def __init__(self, key, val):
         self.key = key
-        self.prev = self.next = None
+        self.val = val
+        self.next = None
+        self.prev= None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.key_to_node = {}
         self.cap = capacity
-        self.cache = {}  # key: node(key, val)
+        self.right = self.left = Node(0, 0)
+        self.left.next = self.left.prev = self.right
+        self.right.next = self.right.prev = self.left
 
-        # left: LRU, right: most recently used
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    # insert node to right
-    def insert(self, node: Node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.prev, node.next = prev, nxt
-
-    # remove node from list
-    def remove(self, node: Node):
+    def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
+    def insert(self, node):  # insert to the left of the right most node
+        prev = self.right.prev
+        prev.next = self.right.prev = node
+        node.next = self.right
+        node.prev = prev
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+        if key in self.key_to_node:
+            self.remove(self.key_to_node[key])
+            self.insert(self.key_to_node[key])
+            return self.key_to_node[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        
-        # evict the least lru if cache size exceeds capacity
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-        
+        if key in self.key_to_node:
+            self.remove(self.key_to_node[key])
+        new_node = Node(key, value)
+        self.key_to_node[key] = new_node
+        self.insert(new_node)
+
+        if len(self.key_to_node) > self.cap:
+            del self.key_to_node[self.left.next.key]
+            self.remove(self.left.next)
