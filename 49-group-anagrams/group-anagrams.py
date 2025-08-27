b2a6eb99c8bab3b896fd5ea85985ff9b226c9b0d
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-08-27 10:47:57 -0500

    Time: 9 ms (89.86%) | Memory: 20.7 MB (76.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)  # mapping char count to string
-
+        res = defaultdict(list)
         for s in strs:
-            key = ''.join(sorted(s))
+            key = "".join(sorted(s))
             res[key].append(s)
         
         return list(res.values())
