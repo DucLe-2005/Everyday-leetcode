788006d725b657ad560fd949c10faa9f5431c455
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-08-29 13:39:36 -0500

    Time: 14 ms (91.21%) | Memory: 23.5 MB (19.15%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -6,11 +6,11 @@ class Solution:
         cur = 0
         start = 0
         n = len(gas)
-        
+
         for i in range(n):
             cur += gas[i] - cost[i]
             if cur < 0:
                 start = i + 1
                 cur = 0
 
-        return start % n
+        return start
