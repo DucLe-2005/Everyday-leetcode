5247a0f6bedc75c8b526eef900d27311a171ba61
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-01 10:53:36 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (8.74%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -2,30 +2,33 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.strip()
         res = 0
-        neg = False
+        n = len(s)
+        sign = 1
+        idx = 0
 
-        if len(s) == 0:
+        if n == 0:
+            return res
+        
+        if s[0] == "-":
+            sign = -1
+            idx = 1
+        elif s[0] == "+":
+            idx = 1
+        elif not s[0].isdigit():
             return res
 
-        i = 0
-        if s[0] == "-":
-            neg = True
-            i = 1
-        elif s[0] == "+":
-            i = 1
+        while idx < n and s[idx].isdigit():
+            res = res * 10 + ord(s[idx]) - ord('0')
+            idx += 1
         
-        while i < len(s) and s[i].isdigit():
-            res = res * 10 + ord(s[i]) - ord('0')
-            i += 1
-        
-        if neg:
-            res = -res
-        
-        INT_MIN, INT_MAX = -2**31, 2**31 - 1
-        if res < INT_MIN:
-            return INT_MIN
-        elif res > INT_MAX:
-            return INT_MAX
+        res = res * sign
+
+        MAX_CAP = 2**31 - 1
+        MIN_CAP = -2**31
+
+        if res > MAX_CAP:
+            return MAX_CAP
+        elif res < MIN_CAP:
+            return MIN_CAP
         else:
             return res
-
