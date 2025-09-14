afabfa98ad35d5604273927e2b8e87bc94577264
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-14 14:42:43 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (27.37%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -1,37 +1,40 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
-        
+    # create a n*n matrix
+    # keep boundaries and traverse through the matrix in spiral order
+    # go top row from right to left, the right column from top to bottom, bottom from right to left, and left column from bottom to top
+    # add number to each block and then increase the number so the next block has a higher number
         a = 1
-        left, right = 0, len(matrix[0])
-        top, bottom = 0, len(matrix)
-        
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        left, right = 0, n
+        top, bottom = 0, n
+
         while left < right and top < bottom:
-            # add numbers to the top row from left to right
-            for i in range(left, right):
+            # top row: left to right
+            for i in range(left, right, 1):
                 matrix[top][i] = a
                 a += 1
             top += 1
 
-            # add numbers to the right column from top to bottom
-            for i in range(top, bottom):
+            # right column: top to bottom
+            for i in range(top, bottom, 1):
                 matrix[i][right - 1] = a
                 a += 1
             right -= 1
 
-            # if not left < right or not top < bottom:
-            #     break
-
-            # add numbers to the bottom row from right to left
+            if not left < right or not top < bottom:
+                break
+            
+            # bottom row: right to left
             for i in range(right - 1, left - 1, -1):
                 matrix[bottom - 1][i] = a
                 a += 1
             bottom -= 1
 
-            # add numbers to the right column from bottom to top
+            # left column: bottom to top
             for i in range(bottom - 1, top - 1, -1):
                 matrix[i][left] = a
                 a += 1
             left += 1
-
+        
         return matrix
