322790a76e7afba01f950c972e2b22ae30d3cb2a
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-15 15:55:43 -0500

    Time: 11 ms (49.30%) | Memory: 18.4 MB (52.66%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,16 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         # keep a running max and min product ending at index i
-        # when encounter 0, set the max and min product ending at index i to 1
+        # update the max and the min by getting the largest value and smallest value from the max * n, min * n, and n itself
         # update the result with current max at each step
 
         res = max(nums)
         currMax, currMin = 1, 1
         for n in nums:
-            if n == 0:   # set max and min to 1 because 1 is neutral
-                currMax, currMin = 1, 1
-                continue
-            
             tmp = currMax * n
             currMax = max(currMax * n, currMin * n, n)
             currMin = min(tmp, currMin * n, n)
