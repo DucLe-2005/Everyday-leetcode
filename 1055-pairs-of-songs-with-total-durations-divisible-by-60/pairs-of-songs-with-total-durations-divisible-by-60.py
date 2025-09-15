de402374bc052cfc93a17590743f175e16a56ce4
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-15 14:52:53 -0500

    Time: 20 ms (66.02%) | Memory: 21.2 MB (61.33%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,10 +1,16 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
+    # use a hash table to store the remainder of each song after dividing it by 60 (use % operation)
+    # if two remainders' sum equals 60, then their numbers' sum equals 60
+    # if a (60 - A_remainder) is in the hash table, add the value to result
+
         res = 0
-        d = [0] * 61   # remainder -> count
+        d = defaultdict(int)
         for t in time:
             r = t % 60
             res += d[60 - r]
-            d[r if r else 60] += 1
+            if r == 0:
+                r = 60
+            d[r] += 1
         
         return res
