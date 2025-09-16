c7da962eb7f9d044e761c083824639f906046535
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-16 13:21:09 -0500

    Time: 96 ms (40.68%) | Memory: 42.7 MB (43.72%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,14 +1,18 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        diagonals = defaultdict(list)
-        for i in range(len(nums)):
-            for j in range(len(nums[i])):
-                diagonals[i+j].append(nums[i][j])
+        # traverse each column form left right from top row to bottom row
+        # add the number to the hash table with indexes sum being the key: (row + colum ): [a, b, c]
+        # after adding all numbers to the has table, append all items in values' arrays to the result
+
+       
+        d = defaultdict(list)
+        for r in range(len(nums)):
+            for c in range(len(nums[r])):
+                d[r + c].append(nums[r][c])
         
         res = []
-        for values in diagonals.values():
-            for n in values[::-1]:
+        for vals in d.values():
+            for n in vals[::-1]:
                 res.append(n)
         
         return res
-
