6b56f92bee52ef313bdd6698aa850d4ed3ee819f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-16 11:49:42 -0500

    Time: 49 ms (72.54%) | Memory: 35.6 MB (42.32%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        d = defaultdict(int)
+        d = Counter(words)
         res = 0
-        for w in words:
+        middle = 0
+
+        for w, f in d.items():
             reverse = w[::-1]
-            if reverse in d and d[reverse] > 0:  # if the palindrome of the word exists, add to result
-                d[reverse] -= 1
-                res += 4
-            else:
-                d[w] += 1
+
+            if reverse == w:  # the word is palindrome itself
+                if f % 2 == 0:
+                    res += f * 2  # if frequency is even, spread the words evenly to 2 sides
+                else:
+                    res += (f - 1) * 2 # if odd, put one in the middle and spread the rest evenly
+                    middle = 1
+            elif w < reverse: # avoid double counting
+                res += min(f, d[reverse]) * 4
         
-        # add them middle value
-        for n, val in d.items():
-            if val > 0 and n[0] == n[1]:  # both characters of the word must be the same
-                return res + 2
-        
-        return res
+        return res + middle * 2
