2f8c4c8e5399bfb1f18ab77141ef11f070a00eac
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-24 16:14:02 -0500

    Time: 36 ms (17.24%) | Memory: 18.3 MB (66.49%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,37 +2,21 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
         rows, cols = len(grid), len(grid[0])
-        visit = set()
 
         def dfs(r, c):
-            cur_area = 1
-            s = []
-            s.append((r, c))
-            visit.add((r, c))
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            while s:
-                row, col = s.pop()
-                for rd, cr in directions:
-                    r, c = row + rd, col + cr
-                    if (r in range(rows) and
-                        c in range(cols) and 
-                        grid[r][c] == 1 and
-                        (r, c) not in visit):
+            if not (r in range(rows) and c in range(cols) and grid[r][c] == 1):
+                return 0
 
-                        s.append((r, c))
-                        visit.add((r, c))
-                        cur_area += 1
-            
-            return cur_area
+            grid[r][c] = 0  # mark this position as visited
+
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and (r, c) not in visit:
-                    print(r, c)
-                    area = dfs(r, c)
-                    res = max(res, area)
+                if grid[r][c] == 1:
+                    res = max(res, dfs(r, c))
         
         return res
         
     # time: O(m *  n)
-    # space: 
+    # space: O(m * n)
