29bfa6a3ee481880e233c914d45e3b47a543e1f6
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-25 13:35:48 -0500

    Time: 7 ms (55.55%) | Memory: 22.1 MB (25.60%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -5,54 +5,38 @@ class Solution:
         """
         # check all sides of the board
         # add all 'O' to a queue
-        # use bfs on on all 'O' in the queues to find regions that are not surrounded, mark those positions as open
+        # use dfs on on all 'O' in the queues to find regions that are not surrounded, mark those positions as open
         # iterate each position on the board to translate 'O' to 'X', except those that are marked as 'open'
 
         # time: O(m * m)
         # space: O(m * n)
     
         m, n = len(board), len(board[0])
-        not_captured = set()
+        isSurrounded = [[True] * n for _ in range(m)]
 
-        q = collections.deque()  # add 'O' on the sides to queue
-        for r in range(m):
-            if board[r][0] == 'O':
-                q.append((r, 0))
-                not_captured.add((r, 0))
-            if board[r][n - 1] == 'O':
-                q.append((r, n - 1))
-                not_captured.add((r, n - 1))
+        def dfs(i, j):
+            if i not in range(m) or j not in range(n) or board[i][j] != 'O' or not isSurrounded[i][j]:
+                return
+            isSurrounded[i][j] = False
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1) 
+
+        for i in range(m):
+            if board[i][0] == 'O':
+                dfs(i, 0)
+            if board[i][n - 1] == 'O':
+                dfs(i, n-1)
         
-        for c in range(n):
-            if board[0][c] == 'O':
-                q.append((0, c))
-                not_captured.add((0, c))
-            if board[m - 1][c] == 'O':
-                q.append((m - 1, c))
-                not_captured.add((m - 1, c))
-                
+        for j in range(n):
+            if board[0][j] == 'O':
+                dfs(0, j)
+            if board[m-1][j] == 'O':
+                dfs(m-1, j)
 
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-        while q:
-            r, c = q.popleft()
-            for dr, dc in dirs:
-                nr, nc = r + dr, c + dc
-                print(nr, nc)
-                if (nr in range(m) and
-                    nc in range(n) and 
-                    (nr, nc) not in not_captured and 
-                    board[nr][nc] == 'O'):
-                    not_captured.add((nr, nc))
-                    q.append((nr, nc))
-        
         for r in range(m):
             for c in range(n):
-                if (r, c) not in not_captured:
+                if board[r][c] == 'O' and isSurrounded[r][c]:
                     board[r][c] = 'X'
-
-        
-
-
-        
         
