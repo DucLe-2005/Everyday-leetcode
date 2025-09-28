9c0e19b9a865b24a4b5e943610a359709abceaeb
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-28 15:11:59 -0500

    Time: 104 ms (89.87%) | Memory: 19.9 MB (79.63%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,34 +1,63 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # 1. multi-source bfs
-        m, n = len(mat), len(mat[0])
-        matrix = [row[:] for row in mat]
-        seen = set()
-        q = collections.deque()
+        # # 1. multi-source bfs
+        # m, n = len(mat), len(mat[0])
+        # matrix = [row[:] for row in mat]
+        # seen = set()
+        # q = collections.deque()
 
-        # add all O's to the queue
+        # # add all O's to the queue
+        # for i in range(m):
+        #     for j in range(n):
+        #         if mat[i][j] == 0:
+        #             q.append((i, j))
+
+        # def valid(x, y):
+        #     return nx in range(m) and ny in range(n) and (nx, ny) not in seen and mat[nx][ny] == 1
+
+        # # bfs simultaneously from O's
+        # directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        # level = 1
+        # while q:
+        #     for _ in range(len(q)):
+        #         x, y = q.popleft()
+        #         for dx, dy in directions:
+        #             nx, ny = x + dx, y + dy
+        #             if valid(nx, ny):
+        #                 q.append((nx, ny))
+        #                 matrix[nx][ny] = level
+        #                 seen.add((nx, ny))
+            
+        #     if q:
+        #         level += 1
+
+        # return matrix
+
+
+        # DP solution
+        dp = [row[:] for row in mat]
+        m, n = len(dp), len(dp[0])
+
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 0:
-                    q.append((i, j))
+                min_neighbor = float("inf")
+                if dp[i][j] != 0:
+                    if i > 0:
+                        min_neighbor = min(min_neighbor, dp[i-1][j])
+                    if j > 0:
+                        min_neighbor = min(min_neighbor, dp[i][j-1])
+        
+                    dp[i][j] = min_neighbor + 1
 
-        def valid(x, y):
-            return nx in range(m) and ny in range(n) and (nx, ny) not in seen and mat[nx][ny] == 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                min_neighbor = float("inf")
+                if dp[i][j] != 0:
+                    if i < m - 1:
+                        min_neighbor = min(min_neighbor, dp[i+1][j])
+                    if j < n - 1:
+                        min_neighbor = min(min_neighbor, dp[i][j+1])
+                
+                    dp[i][j] = min(dp[i][j], min_neighbor + 1)
 
-        # bfs simultaneously from O's
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        level = 1
-        while q:
-            for _ in range(len(q)):
-                x, y = q.popleft()
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-                    if valid(nx, ny):
-                        q.append((nx, ny))
-                        matrix[nx][ny] = level
-                        seen.add((nx, ny))
-            
-            if q:
-                level += 1
-
-        return matrix
+        return dp 
