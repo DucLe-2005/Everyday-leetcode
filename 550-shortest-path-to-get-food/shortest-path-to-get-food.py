52930c3a1bf9f59970751e86ef7aa09eced10eae
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-09-28 16:24:14 -0500

    Time: 75 ms (66.59%) | Memory: 26.9 MB (13.73%) - LeetSync

--- 550-shortest-path-to-get-food/shortest-path-to-get-food.py
+++ 550-shortest-path-to-get-food/shortest-path-to-get-food.py
@@ -1,11 +1,5 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        # put all #'s into a queue
-        # conduct a bfs simultaneously on all the food
-        # each time we expand out, the lenght increments by 1.
-        # if we reached "*", then return the length.
-        # if the queue is still empty, the return -1.
-
         m, n = len(grid), len(grid[0])
         q = collections.deque()
         seen = set()
@@ -13,8 +7,11 @@ class Solution:
         # put all food's locations to the queue
         for r in range(m):
             for c in range(n):
-                if grid[r][c] == '#':
+                if grid[r][c] == '*':
                     q.append((r, c))
+                    break
+            if q:
+                break
 
         # bfs simultaneously starting from all food's locations
         length = 0
@@ -22,18 +19,17 @@ class Solution:
         while q:
             for _ in range(len(q)):
                 r, c = q.popleft()
+                if grid[r][c] == "#":
+                    return length
+
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    
-                    if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in seen:
-                        if grid[nr][nc] == '*':
-                            return length + 1
-                        elif grid[nr][nc] == 'O':
-                            q.append((nr, nc))
-                            seen.add((nr, nc))
+                    if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in seen and grid[nr][nc] != 'X':
+                        q.append((nr, nc))
+                        seen.add((nr, nc))
             
             length += 1
-            
+
         return -1
 
 
