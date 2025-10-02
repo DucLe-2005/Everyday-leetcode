9b596fd5f72c5d88a4db6aaf540eafd2777520b9
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-02 16:43:35 -0500

    Time: 391 ms (57.21%) | Memory: 17.7 MB (83.07%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,37 +1,38 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res = []
-        nums.sort()
         n = len(nums)
-
         if n < 4:
-            return res
-        
-        for i in range(n - 3):
-            # skip duplicates on i
-            if i > 0 and nums[i] == nums[i-1]:
+            return []
+
+        nums.sort()
+        res = []
+        for a in range(n):
+            if a > 0 and nums[a] == nums[a-1]:
                 continue
             
-            for j in range(i + 1, n - 2):
-                l, r = j + 1, n - 1
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for b in range(a+1, n):
+                if b > a + 1 and nums[b] == nums[b-1]:
                     continue
-
-                while l < r:
-                    total = nums[i] + nums[j] + nums[l] + nums[r]
-
+                
+                c, d = b + 1, n - 1
+                while c < d:
+                    total = nums[a] + nums[b] + nums[c] + nums[d]
                     if total == target:
-                        res.append([nums[i], nums[j], nums[l], nums[r]])
-                        
-                        l += 1
-                        r -= 1
-                        while l < r and nums[l] == nums[l - 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r + 1]:
-                            r -= 1
-                    elif total > target:
-                        r -= 1
-                    else:
-                        l += 1
+                        res.append([nums[a], nums[b], nums[c], nums[d]])
 
+                        c += 1
+                        while c < d and nums[c] == nums[c-1]:
+                            c += 1
+                        
+                        d -= 1
+                        while c < d and nums[d] == nums[d+1]:
+                            d -= 1
+
+                    elif total < target:
+                        c += 1
+                    else:
+                        d -= 1
+        
         return res
+                
+
