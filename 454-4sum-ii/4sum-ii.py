ef33ef0b7d0a937177b4ea3fc4324d9447572a8e
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-02 16:54:04 -0500

    Time: 343 ms (50.44%) | Memory: 18.1 MB (36.20%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -2,18 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        # create a hash table to store occurences of sums made from nums1 and nums2
-        # for all sums made from nums3 and nums4, check if (0 - num3 - num3) in the hash table
-        # if yes, then add the value to result
-
-        d = defaultdict(int)
-        for n1 in nums1:
-            for n2 in nums2:
-                d[n1 + n2] += 1
-        
+        val_map= defaultdict(int)
+        n = len(nums1)
         res = 0
-        for n3 in nums3:
-            for n4 in nums4:
-                res += d[0 - n3 - n4]
+        for n1 in range(n):
+            for n2 in range(n):
+                val_map[nums1[n1] + nums2[n2]] += 1
+        
+        for n3 in range(n):
+            for n4 in range(n):
+                res += val_map[-nums3[n3] - nums4[n4]]
 
-        return res 
+        return res
+
+                    
