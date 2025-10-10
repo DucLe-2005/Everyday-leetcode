785d87ec32c2d4fc003eccd6a0697a4e1d783d0d
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-09 22:10:21 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (31.51%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,12 +1,34 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         # recurive binary exponentiation
+        # if n == 0:
+        #     return 1.0
+        # if n < 0:
+        #     return 1.0 / self.myPow(x , -1 * n)
+        
+        # if n % 2 == 0:
+        #     return self.myPow(x * x, n // 2)
+        # else:
+        #     return x * self.myPow(x * x, (n - 1) // 2)
+
+        # iterative binary exponentiation
         if n == 0:
             return 1.0
         if n < 0:
-            return 1.0 / self.myPow(x , -1 * n)
+            x = 1 / x 
+            n *= -1
+
+        res = 1
+        while n > 0:
+            if n % 2 != 0:
+                res *= x
+                n -= 1
+            
+            x *= x
+            n //= 2
         
-        if n % 2 == 0:
-            return self.myPow(x * x, n // 2)
-        else:
-            return x * self.myPow(x * x, (n - 1) // 2)
+        return res
+            
+
+
+        
