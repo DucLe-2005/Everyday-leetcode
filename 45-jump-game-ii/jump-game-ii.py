61b0c37eb75aba37c66790b05d1dcf1127a7437f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-24 16:09:42 -0500

    Time: 11 ms (44.08%) | Memory: 18.5 MB (74.73%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -4,14 +4,13 @@ class Solution:
         if n == 1:
             return 0
         
-        farthest = nums[0]
         current_end = nums[0]
+        farthest = 0
         jumps = 1
+        for i in range(n-1): # stop at the last index
+            farthest = max(farthest, i + nums[i])
 
-        for i in range(len(nums) - 1):
-            farthest = max(farthest, nums[i] + i)
-            
-            if i == current_end:
+            if i == current_end: # jump to the next farthest spot
                 current_end = farthest
                 jumps += 1
         
