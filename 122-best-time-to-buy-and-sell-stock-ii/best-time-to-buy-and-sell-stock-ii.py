31172037eaefc9f71803fbeadf060556b76766fe
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-24 15:18:17 -0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (98.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,23 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # State: buy and sell
-        # If buy, buy or skip
-        # If sell, sell or skip
-        dp = {}  # (i, buying): profit
-        
-        def dfs(i, buying):
-            if i >= len(prices):
-                return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-            
-            skip = dfs(i+1, buying)
-            if buying:
-                amount = max(skip, dfs(i+1, not buying) - prices[i])
-            else:
-                amount = max(skip, prices[i] + dfs(i+1, not buying))
-            
-            dp[(i, buying)] = amount
-            return dp[(i, buying)]
-        
-        return dfs(0, True)
+        res = 0
+        for i in range(len(prices)):
+            if i > 0 and prices[i] > prices[i-1]:
+                res += prices[i] - prices[i-1]
+        return res
