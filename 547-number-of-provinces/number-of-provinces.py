35112697198ac5a3e26b1944887979d01f0fd296
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-10-31 23:22:47 -0500

    Time: 255 ms (5.11%) | Memory: 19.7 MB (5.91%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,39 +1,23 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        self.res = 0
         n = len(isConnected)
-        self.parent = [i for i in range(n)]
-        self.rank = [0] * n
 
-        def find(x):
-            if self.parent[x] != x:
-                self.parent[x] = find(self.parent[x])
-            return self.parent[x]
-
-        def union(x, y):
-            rootX = find(x)
-            rootY = find(y)
-
-            if rootX == rootY:
+        def dfs(x, y):
+            if not isConnected[x][y]:
                 return
             
-            if self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            elif self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            else:
-                self.parent[rootX] = rootY
-                self.rank[rootY] += 1
+            isConnected[x][y] = 0
 
-
+            for i in range(n):
+                dfs(y, i)
+            
+            
+        
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j]:
-                    union(i, j)
+                    dfs(i, j)
+                    self.res += 1
         
-        
-        roots = {find(i) for i in self.parent}
-        return len(roots)
-
-        
-    
-    
+        return self.res
