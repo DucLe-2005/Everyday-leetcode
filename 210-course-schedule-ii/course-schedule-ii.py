e2c3730e3a2a1f8295972fa5212726a52830c566
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-03 10:46:41 -0600

    Time: 4 ms (47.93%) | Memory: 19 MB (84.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,34 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-       # Build graph: pre -> course
         graph = defaultdict(list)
-        for course, pre in prerequisites:
-            graph[pre].append(course)
+        indegree = [0] * numCourses
+        for child, parent in prerequisites:
+            graph[parent].append(child)
+            indegree[child] += 1
         
-        color = [0] * numCourses  # 0=unvisited, 1=visiting, 2=done
-        order = []               # post order
-
-        def dfs(u):
-            if color[u] == 1:    # cycle
-                return False
-            if color[u] == 2:    # already processed
-                return True
+        zeroDegree = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                zeroDegree.append(i)
             
-
-            color[u] = 1
-            for v in graph[u]:
-                if not dfs(v):
-                    return False
-
-            color[u] = 2
-            order.append(u)
-            return True
+        res = []
+        while zeroDegree:
+            course = zeroDegree.popleft()
+            res.append(course)
+            
+            for child in graph[course]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    zeroDegree.append(child)
         
-        for u in range(numCourses):
-            if color[u] == 0:
-                if not dfs(u):
-                    return []
+        for i in indegree:
+            if i != 0:
+                return []
+        return res
+        
 
-        return order[::-1]
 
        
