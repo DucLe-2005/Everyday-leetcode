b2495cc15a3005885e69d691991deafa44695fdd
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-05 16:02:22 -0600

    Time: 743 ms (48.56%) | Memory: 82.5 MB (19.18%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        @lru_cache(maxsize=None)
+        def dfs(nums, n, subset_sum):
+            if subset_sum == 0:
+                return True
+            
+            if n == 0 or subset_sum < 0:
+                return False
+            
+            return dfs(nums, n - 1, subset_sum - nums[n - 1]) or dfs(nums, n - 1, subset_sum)
+
+        
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
+            return False
+        
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(tuple(nums), n, subset_sum)
+
+
+
+
