fb3e398bf85873af934addcdf21809457073746f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-06 17:00:37 -0600

    Time: 496 ms (33.25%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,3 @@
--- select w1.id
--- from weather w1
--- join weather w2 on datediff(w1.recordDate, w2.recordDate) = 1
--- and w1.temperature > w2.temperature;
-
-select w1.id
-from weather w1, weather w2
-where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
