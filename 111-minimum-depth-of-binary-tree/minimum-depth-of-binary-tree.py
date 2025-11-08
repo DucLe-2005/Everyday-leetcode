6e8fc03656e8addc1944b4334e37c8302e74c5aa
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-08 14:16:49 -0600

    Time: 2 ms (81.18%) | Memory: 49.8 MB (86.51%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,20 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        q = collections.deque([root])
-        depth = 1
+        
+        q = collections.deque([(root, 1)])
+
         while q:
-            qSize = len(q)
-            for _ in range(qSize):
-                node = q.popleft()
-                # Since we added nodes without checking null, we need to skip them here.
-                if not node:
-                    continue
-                # The first leaf would be at minimum depth, hence return it.
-                if not node.left and not node.right:
-                    return depth
-                q.append(node.left)
-                q.append(node.right)
-            depth += 1
-        return -1
+            size = len(q)
+            for _ in range(size):
+                node, count = q.popleft()
+                if not node.right and not node.left:
+                    return count
+                if node.left:
+                    q.append((node.left, count + 1))
+                if node.right:
+                    q.append((node.right, count + 1))
