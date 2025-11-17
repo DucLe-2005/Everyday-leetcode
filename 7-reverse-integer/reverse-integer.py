41a6222474b90cb0db2e629634243a0538d4aa8e
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-17 15:42:10 -0600

    Time: 39 ms (58.53%) | Memory: 17.8 MB (57.36%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,15 +3,21 @@ class Solution:
         if x == 0:
             return 0
 
-        sign = 1 if x >0 else - 1
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        sign = 1 if x > 0 else - 1
         res = 0
         x = -x if x < 0 else x
 
         while x:
             digit = x % 10
-            res = res * 10 + digit
-            if res > 2**31 - 1:
-                return 0
             x //= 10
+            if sign == 1 and res > INT_MAX // 10 or (res == INT_MAX // 10 and digit > 7):
+                return 0
+            if sign == -1 and res > -INT_MIN // 10 or (res == -INT_MIN // 10 and digit > 8):
+                return 0
+
+            res = res * 10 + digit
         
         return res * sign
