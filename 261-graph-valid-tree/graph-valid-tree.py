b9b7eabea69bf407e408a1df99f91cc6aca1e470
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-11-30 17:37:42 -0600

    Time: 3 ms (47.33%) | Memory: 20 MB (10.66%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,28 +1,28 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        # create an adjacency list out of the edges
-        # do a dfs on the neighbors of each node
+        # create an adjacency list. key: node, values: list of neighbors
+        # create a visit set
+        # if node is already in visit set, then a cycle is detected and it is not a valid tree
 
-        adj = defaultdict(list)
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
-    
-        visit = set()  # all visisted nodes along the dfs path
+        adj = {i: [] for i in range(n)}
+
+        for node1, node2 in edges:
+            adj[node1].append(node2)
+            adj[node2].append(node1)
+
+        visited = set()
+
 
         def dfs(node, prev):
-            if node in visit:
+            if node in visited:
                 return False
             
-            visit.add(node)
+            visited.add(node)
 
             for neighbor in adj[node]:
-                if neighbor == prev:
-                    continue
-                if not dfs(neighbor, node):
+                if neighbor != prev and not dfs(neighbor, node):
                     return False
             
             return True
         
-        return dfs(0, -1) and len(visit) == n
-
+        return dfs(0, -1) and len(visited) == n
