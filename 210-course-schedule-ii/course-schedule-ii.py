dcf47e451324d1bb952b92fbaa7f0cbf0cf3c284
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-02 17:58:32 -0600

    Time: 3 ms (77.10%) | Memory: 18.9 MB (84.11%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,38 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        indegree = [0] * numCourses
+        graph = defaultdict(list) 
+        # key: prerequisite course,  
+        # values: list of courses that need this prereq
+        indegree = [0] * numCourses  # number of prerequisites for each course
+
+        # build the graph and indegree array
         for child, parent in prerequisites:
             graph[parent].append(child)
             indegree[child] += 1
         
-        zeroDegree = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                zeroDegree.append(i)
-            
-        res = []
-        while zeroDegree:
-            course = zeroDegree.popleft()
-            res.append(course)
-            
+        # Add course with no prerequisite to queue
+        zero_queue = deque()
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                zero_queue.append(course)
+        
+        # Use BFS to determine the course order
+        course_order = []
+        while zero_queue:
+            course = zero_queue.popleft()
+            course_order.append(course)
+
+            # Decrease the indegree of the course's children by 1
             for child in graph[course]:
                 indegree[child] -= 1
                 if indegree[child] == 0:
-                    zeroDegree.append(child)
+                    zero_queue.append(child)
         
-        for i in indegree:
-            if i != 0:
-                return []
-        return res
-        
+        # There is a cycle, so it is impossible to finish all courses
+        if numCourses != len(course_order):
+            return []
+        return course_order
 
 
-       
+        # time: O(V + E)
+        # space: O(V + E)
