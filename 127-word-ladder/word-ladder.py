692ee552610d9552b630f920a45041ba50a54e37
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-04 14:44:01 -0600

    Time: 27 ms (98.80%) | Memory: 18.9 MB (74.34%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -7,33 +7,41 @@ class Solution:
         endSet = {endWord}
         length = 1
 
+        # bidirectional bfs from beginSet and endSet to find the shortest path
         while beginSet and endSet:
-            if len(beginSet) > len(endSet):
-                temp = beginSet
+            if len(endSet) < len(beginSet):
+                tmp = beginSet
                 beginSet = endSet
-                endSet = temp
-            newBeginSet = set()
+                endSet = tmp
+            
+            newSet = set()
             for word in beginSet:
                 neighbors = self.neighbors(word)
-                for neigh in neighbors:
-                    if neigh in endSet:
+                for neighbor in neighbors:
+                    if neighbor in endSet:
+                        print(beginSet)
+                        print(endSet)
                         return length + 1
-                    if neigh in words:
-                        words.remove(neigh)
-                        newBeginSet.add(neigh)
-            beginSet = newBeginSet
+                    if neighbor in words:
+                        print(neighbor)
+                        newSet.add(neighbor)
+                        words.remove(neighbor)  # avoid cycle
+            
+            beginSet = newSet
             length += 1
+
+        return 0  # we can't find a path between two words
         
-        return 0
-    
-    def neighbors(self, s: str) -> List[str]:
-        chars = list(s)
+    def neighbors(self, word):
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        chars = list(word)
         res = []
-        for i in range(len(chars)):
-            temp = chars[i]
+        
+        for i in range(len(word)):
+            c = chars[i]
             for a in alphabet:
                 chars[i] = a
                 res.append("".join(chars))
-            chars[i] = temp
+            chars[i] = c
+        
         return res
