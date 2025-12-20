e6053ca4fb7b6bd0b81659c6ddd59f5323d52ae5
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-20 12:43:45 -0600

    Time: 0 ms (100.00%) | Memory: 17.3 MB (99.82%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        # p1: points to the node lower than x
+        # p2: points to the node higher than x
+        # h: marks the head of higher partition
+        # curr: iterate each node in linked list
+        # if curr < p1, p1.next = curr
+        # if curr > p2, p2.next = curr
+        # if curr reaches the end, p1.next = h
+
+        if not head:
+            return None
+        
+        p1, p2 = None, None
+        h1, h2 = None, None
+        curr = head
+        print("1")
+        while curr:
+            if curr.val < x:
+                if not p1:
+                    p1 = curr
+                    h1 = p1
+                else:
+                    p1.next = curr
+                    p1 = curr
+            else:
+                if not p2:
+                    p2 = curr
+                    h2 = p2
+                else:
+                    p2.next = curr
+                    p2 = curr
+            curr = curr.next
+        
+        # Return head if either of the parition is empty
+        if not h1 or not h2:
+            return head
+        
+        p1.next = h2
+        p2.next = None
+        return h1
+
+        
+        
+                
+
+
+        
+
+
