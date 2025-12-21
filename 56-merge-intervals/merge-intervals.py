62630c0aabfea54e9c31cb2edfabeef21853395e
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-21 11:58:11 -0600

    Time: 11 ms (30.40%) | Memory: 21.6 MB (32.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,18 +4,17 @@ class Solution:
         # 2. keep res array
         # 3. iterate intervals
         # 4. check if the current interval overlaps with the last interval
-        # that we added toe res
         # 5. if yes, update the ending value of the last added interval using max()
         # 6. if no, add this current interval to the res
 
         intervals.sort()
         res = [intervals[0]]
-        for s1, e1 in intervals[1:]:
-            s2, e2 = res[-1]
-            if s1 <= e2:
-                res[-1][1] = max(e1, e2)
+        for curr_start, curr_end in intervals[1:]:
+            prev_start, prev_end = res[-1]
+            if curr_start <= prev_end:
+                res[-1][1] = max(prev_end, curr_end)
             else:
-                res.append([s1, e1])
+                res.append([curr_start, curr_end])
         
         return res
         
