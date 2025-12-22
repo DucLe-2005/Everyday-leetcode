7fe5e2db4f30ae02f0a8596f6010313d1b4f97c9
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-22 16:37:17 -0600

    Time: 59 ms (74.65%) | Memory: 26.4 MB (96.40%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        maxP = 0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
+        # res = the maximum profit
+        # lowest = the lowest price that we've seen so far
+        # iterate the day in prices
+        # if I encounter a number > lowest, update res if (number - lowest) > res
+        # if I encounter a number < lowest, update lowest = number
+        # return res once reaches all numbers
+        # time: O(n)  
+        # space: O(1)
+
+        res = 0
+        lowest = prices[0]
+        for p in prices:
+            if p >= lowest:
+                res = max(res, p - lowest)
             else:
-                l = r
-            r += 1
-        return maxP
+                lowest = p
+        
+        return res
