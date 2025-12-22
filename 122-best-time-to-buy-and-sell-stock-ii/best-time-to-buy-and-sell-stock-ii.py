d35d414da15f1d34bd6ee59d5a58942ae65e7917
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-22 17:33:35 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (96.63%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,20 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        peak = prices[0]
-        valey = prices[0]
-        i = 0
-        n = len(prices)
-        profit = 0
-        while i < n - 1:
-            while i < n - 1 and prices[i] >= prices[i + 1]:
-                i += 1
-            valey = prices[i]
-
-            while i < n - 1 and prices[i] <= prices[i + 1]:
-                i += 1
-            peak = prices[i]
-            
-            profit += peak - valey
+        res = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                res += prices[i + 1] - prices[i]
         
-        return profit
-        
+        return res
