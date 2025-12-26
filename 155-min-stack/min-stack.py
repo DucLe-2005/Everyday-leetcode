d09d518f14b3a3d7afdd0d79b2be3c5ccff718e6
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-26 17:29:40 -0600

    Time: 2211 ms (5.12%) | Memory: 20.6 MB (93.90%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,16 +2,35 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min_stack = []
+        self.min_stack = []  # monotonic increasing stack
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        
+        tmp = []
+        while self.min_stack and self.min_stack[-1] < val:
+            tmp.append(self.min_stack.pop())
         self.min_stack.append(val)
+        
+        while tmp:
+            self.min_stack.append(tmp.pop())
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.min_stack.pop()
+        pop = self.stack.pop()
+
+        
+        if self.min_stack[-1] == pop:
+            self.min_stack.pop()
+        else:
+            tmp = []
+            while self.min_stack[-1] != pop:
+                tmp.append(self.min_stack.pop())
+            self.min_stack.pop()
+
+            while tmp:
+                self.min_stack.append(tmp.pop())
+        
+        return pop
 
     def top(self) -> int:
         return self.stack[-1]
