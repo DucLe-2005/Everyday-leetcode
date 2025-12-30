d10e345748dd9483bf335874cab9f26e0f404711
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-30 11:35:29 -0600

    Time: 11 ms (31.09%) | Memory: 20.9 MB (62.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -8,7 +8,7 @@ class Solution:
         for curr_start, curr_end in intervals[1:]:
             prev_start, prev_end = res[-1]
             if prev_end >= curr_start:
-                res[-1] = [min(curr_start, prev_start), max(curr_end, prev_end)]
+                res[-1][1] = max(curr_end, prev_end)
             else:
                 res.append([curr_start, curr_end])
         
