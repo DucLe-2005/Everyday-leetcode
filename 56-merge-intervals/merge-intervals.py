4c4b55d9952415eb64f7f34fd12c12247e0130bd
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-30 11:34:40 -0600

    Time: 10 ms (35.50%) | Memory: 21.3 MB (39.42%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # 1. sort the intervals
-        # 2. keep res array
-        # 3. iterate intervals
-        # 4. check if the current interval overlaps with the last interval
-        # 5. if yes, update the ending value of the last added interval using max()
-        # 6. if no, add this current interval to the res
-
+        if len(intervals) == 1:
+            return intervals
+        
         intervals.sort()
         res = [intervals[0]]
         for curr_start, curr_end in intervals[1:]:
             prev_start, prev_end = res[-1]
-            if curr_start <= prev_end:
-                res[-1][1] = max(prev_end, curr_end)
+            if prev_end >= curr_start:
+                res[-1] = [min(curr_start, prev_start), max(curr_end, prev_end)]
             else:
                 res.append([curr_start, curr_end])
         
         return res
-        
