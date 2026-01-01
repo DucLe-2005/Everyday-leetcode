355f353342929d4d8e57281cf5e4308109cf7d90
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2025-12-31 21:46:48 -0600

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.88%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,46 +1,31 @@
 class HitCounter:
-    # queue: sums of hits in the recent 300 seconds
-    # hit: add to queue, if number of hits exceed cap, pop hit in the front
-    # getHits: return the sum
 
     def __init__(self):
         self.count = 0
-        self.totalHits = 0
-        self.currentHits = 0
-        self.currentTime = 0
-        self.queue = collections.deque()  # (number of hits, timestamp)
-        
+        self.current_time = 0
+        self.past_hits = deque()
 
     def hit(self, timestamp: int) -> None:
-        if self.currentTime == timestamp:
-            self.currentHits += 1
-            self.totalHits += 1
-        else:
-            self.queue.append((self.currentHits, self.currentTime))
-
-            self.totalHits += 1
-            self.currentTime = timestamp
-            self.currentHits = 1
-
-            self._prune(timestamp)
-            
+        self.past_hits.append(timestamp)
+        self.current_time = timestamp
+        self.count += 1
+        self.update_queue()
 
     def getHits(self, timestamp: int) -> int:
-        if timestamp > self.currentTime:
-            self.queue.append((self.currentHits, self.currentTime))
-            self.currentHits = 0
-        self.currentTime = timestamp
-        self._prune(timestamp)
-        return self.totalHits
+        self.current_time = timestamp
+        self.update_queue()
+        return self.count
     
-    def _prune(self, now):
-        while self.queue and now - self.queue[0][1] >= 300:
-                hits, t = self.queue.popleft()
-                self.totalHits -= hits
-
-
-            
-        
+    def update_queue(self):
+        print("current time", self.current_time)
+        print(self.past_hits)
+        while (
+            self.past_hits and
+            self.current_time - self.past_hits[0] >= 300
+            ):
+            print("pop:", self.past_hits[0])
+            self.count -= 1
+            self.past_hits.popleft() 
 
 
 # Your HitCounter object will be instantiated and called as such:
