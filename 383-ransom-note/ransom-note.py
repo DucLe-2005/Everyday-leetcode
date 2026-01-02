cfe5b9564e3c094873b44fd06fa07e17cf6f4eb8
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-01 21:09:32 -0600

    Time: 15 ms (77.95%) | Memory: 17.7 MB (93.19%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,13 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_count = Counter(ransomNote)
         magazine_count = Counter(magazine)
 
-        for letter in ransom_count:
-            if (
-                letter not in magazine_count or
-                ransom_count[letter] > magazine_count[letter]
-            ):
+        for letter in ransomNote:
+            if letter not in magazine_count or magazine_count[letter] == 0:
                 return False
+            else:
+                magazine_count[letter] -= 1
         
         return True
