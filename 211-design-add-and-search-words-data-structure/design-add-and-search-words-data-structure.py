ce996084267992b98bdc5de79e66134d9a45acc1
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-10 22:00:48 -0600

    Time: 1343 ms (35.77%) | Memory: 68.5 MB (12.39%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,35 +1,41 @@
 class Node:
     def __init__(self):
-        self.children: [str, Node()] = {}
+        self.children = {}
         self.is_end = False
 
 class WordDictionary:
+
     def __init__(self):
         self.root = Node()
 
     def addWord(self, word: str) -> None:
-        curr = self.root
+        cur = self.root
         for ch in word:
-            if ch not in curr.children:
-                curr.children[ch] = Node()
-            curr = curr.children[ch]
-        curr.is_end = True
+            if ch not in cur.children:
+                cur.children[ch] = Node()
+            cur = cur.children[ch]
+        cur.is_end = True
 
     def search(self, word: str) -> bool:
-        def dfs(i, node):
+        print("search", word)
+        def dfs(root, i):
             if i == len(word):
-                return node.is_end
-
-            ch = word[i]
-
-            if ch == ".":
-                for child in node.children.values():
-                    if dfs(i + 1, child):
+                return root.is_end
+            
+            print(word[i])
+            if word[i] == ".":
+                for child in root.children.values():
+                    if dfs(child, i+1):
                         return True
                 return False
-            else:
-                if ch not in node.children:
-                    return False
-                return dfs(i + 1, node.children[ch])
+            if word[i] not in root.children:
+                return False
+            
+            return dfs(root.children[word[i]], i+1)
 
-        return dfs(0, self.root)
+        return dfs(self.root, 0)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
