dc7c575492760db73ac9d608a2754ab50dc8d13f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-13 15:29:21 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (8.30%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1 = 0
-        rob2 = 0
-        # [rob1, rob2, n, n+1, ...]
-        for n in nums:
-            newRob = max(rob1 + n, rob2)
-            rob1 = rob2
-            rob2 = newRob
+        # the total amount of money I have when at house i is:
+        # max amount of money I had when at 2 houses back plus this house i
+        # max amount of money I had when at the previous house
+        # time: O(n), space: O(1)
+        if len(nums) == 1:
+            return nums[0]
+
+        amount_1 = 0
+        amount_2 = 0
+        curr = 0
         
-        return rob2
+        for money in nums:
+            curr = max(amount_1, amount_2 + money)
+            amount_2 = amount_1
+            amount_1 = curr
+
+        return curr
