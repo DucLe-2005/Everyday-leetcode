06136a0cb386fea7332d2e55f23f9192da117f34
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-13 16:29:22 -0600

    Time: 7 ms (83.40%) | Memory: 19.7 MB (11.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,14 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 1
+        # Reasoning: sub[i] stores the minimum possible tail of an increasing subsequence of length i + 1. Appending increases the best-known length only when we can extend an existing subsequence. Replacing improves tails withouth changing the best-known length. Therefore, the length of sub at the end equals the LIS length.
+        sub = [nums[0]]
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                i = 0
+                while sub[i] < num:
+                    i += 1
+                sub[i] = num
         
-        dp = [1] * len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[j] + 1, dp[i])
-        
-        return max(dp)
-
+        return len(sub)
         
