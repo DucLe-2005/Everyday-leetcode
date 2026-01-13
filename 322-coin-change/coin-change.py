74c4859c5d8550583e943c37084e6b1792d8bd23
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-13 15:05:52 -0600

    Time: 629 ms (87.55%) | Memory: 19.8 MB (30.97%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,11 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+        # Reasoning: 
+        # Since amount 7 = amount 5 + coin 2,
+        # the least # of coins for 7 = the least # of coins to make up 5 plus 1
+        # dp[i] = dp[i - coin] + 1
+        # time: O(n * m), space: O(n), n = amount, m( = # of coins
 
-        for i in range(1, amount + 1, 1):
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
             for c in coins:
-                if i - c >= 0:
-                    dp[i] = min(dp[i], 1 + dp[i - c])
-        
-        return dp[amount] if dp[amount] != (amount + 1) else -1
+                if i - c >= 0 and dp[i - c] != -1:
+                    if dp[i] == -1:
+                        dp[i] = dp[i - c] + 1
+                    else:
+                        dp[i] = min(dp[i], dp[i - c] + 1)
+
+        return dp[-1]
