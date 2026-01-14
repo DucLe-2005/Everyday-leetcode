3404ea32cd6a899f21789468c802aee4842b18ed
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-14 13:05:53 -0600

    Time: 636 ms (56.15%) | Memory: 19.3 MB (57.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,25 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # There are two decisions we can make to a number: include or not include
-        # dp[i][j] = True if sum j can be formed by array elements in subset nums[0] -> nums[i]
-        # if dp[i-1][j] = True, then dp[i][j] = True -> not include nums[i]
-        # if dp[i-1][j - nums[i]] = true, then dp[i][j] = true -> include nums[i]
-
+        # dp[i] = True if sum i can be formed from a subset in nums
         total_sum = sum(nums)
+
         if total_sum % 2 != 0:
             return False
-
+        
         subset_sum = total_sum // 2
-        dp = [[False] * (subset_sum+1) for _ in range(len(nums) + 1)]
         
-        dp[0][0] = True
-
-        for i in range(1, len(nums) + 1):
-            curr = nums[i - 1] 
-            for j in range(subset_sum + 1):
-                if j < curr:
-                    dp[i][j] = dp[i-1][j]
-                else: 
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j - curr]
+        dp = [False] * (subset_sum + 1)
+        dp[0] = True
+        for curr in nums:
+            for j in range(subset_sum, curr - 1, -1):
+                # sum j can be formed if previously sum j can be formed
+                # before we consider curr or previously sum j - curr can be formed
+                dp[j] = dp[j] or dp[j - curr]
         
-        return dp[-1][-1]
+        return dp[-1]
