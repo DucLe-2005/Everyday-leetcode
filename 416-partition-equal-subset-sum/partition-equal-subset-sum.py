1f63b29b691d978acfb67f368fdaeeaff362a2f9
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-01-14 12:48:51 -0600

    Time: 1302 ms (34.11%) | Memory: 34.9 MB (28.47%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,26 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
-        @lru_cache(maxsize=None)
-        def dfs(nums, n, subset_sum):
-            if subset_sum == 0:
-                return True
-            
-            if n == 0 or subset_sum < 0:
-                return False
-            
-            return dfs(nums, n - 1, subset_sum - nums[n - 1]) or dfs(nums, n - 1, subset_sum)
+        # There are two decisions we can make to a number: include or not include
+        # dp[i][j] = True if sum j can be formed by array elements in subset nums[0] -> nums[i]
+        # if dp[i-1][j] = True, then dp[i][j] = True -> not include nums[i]
+        # if dp[i-1][j - nums[i]] = true, then dp[i][j] = true -> include nums[i]
 
-        
         total_sum = sum(nums)
-
         if total_sum % 2 != 0:
             return False
-        
+
         subset_sum = total_sum // 2
-        n = len(nums)
-        return dfs(tuple(nums), n, subset_sum)
+        dp = [[False] * (subset_sum+1) for _ in range(len(nums) + 1)]
+        
+        dp[0][0] = True
 
-
-
-
+        for i in range(1, len(nums) + 1):
+            curr = nums[i - 1] 
+            for j in range(subset_sum + 1):
+                if j < curr:
+                    dp[i][j] = dp[i-1][j]
+                else: 
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j - curr]
+        
+        return dp[-1][-1]
