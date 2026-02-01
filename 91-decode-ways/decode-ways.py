8190010c6c9a2dcd7a116e9192855743bfa80ddb
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-02-01 00:15:29 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (32.04%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,17 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n = len(s)
-        decode1, decode2 = 1, 0
-        # [i, decode1, decode2]
-        for i in range(n - 1, -1, -1):
-            newDecode = 0
-            if s[i] != "0":
-                newDecode = decode1
-            if (i + 1 < n and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456")):
-                newDecode += decode2
-            
-            decode2 = decode1
-            decode1 = newDecode
+        dp = [1] * (len(s) + 2)
+        dp[-1] = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+                continue
+            dp[i] = dp[i+1]
+            if (s[i] == "1" or (s[i] == "2" and i + 1 < len(s) and s[i+1] in "0123456")):
+                dp[i] += dp[i+2]
         
-        return decode1
-            
+        return dp[0]
+
+        # time complexity: O(n)   
+        # space complexity: O(n)
