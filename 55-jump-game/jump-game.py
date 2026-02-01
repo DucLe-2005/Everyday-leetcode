1e3da080f3c58cdc1612af1756834019c01cd3cd
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-02-01 01:21:11 -0600

    Time: 17 ms (70.10%) | Memory: 20.5 MB (18.68%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,14 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # within the range of the jump range
-        # choose the landing position that allows you to jump the farthest the next time you jump
-
+        # 1. Greedy: continuously update the farthest jumpable index
+        # time complexity: O(n), space complexity: O(1)
         farthest = 0
         for i in range(len(nums)):
             if i > farthest:
                 return False
             
             farthest = max(farthest, i + nums[i])
+
         return True
-        
-            
