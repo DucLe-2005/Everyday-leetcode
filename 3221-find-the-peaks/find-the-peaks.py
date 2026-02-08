6e48fb3cf2fbae9c0e6c8d33ad1c0d098172ea3c
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-02-07 20:21:17 -0600

    Time: 3 ms (19.58%) | Memory: 19.4 MB (12.46%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.py
+++ 3221-find-the-peaks/find-the-peaks.py
@@ -1,18 +1,12 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
+        if len(mountain) < 3:
+            return []
+        
         res = []
-        curr_peak = 0
-        new_peak = False
-        for i in range(1, len(mountain)):
-            if mountain[i] < mountain[i-1]:
-                if new_peak:
-                    res.append(curr_peak)
-                    new_peak = False
-            elif mountain[i] > mountain[i-1]:
-                curr_peak = i
-                new_peak = True
-            else:
-                new_peak = False
-                
+        for i in range(1, len(mountain) - 1):
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+                res.append(i)
+        
         return res
             
