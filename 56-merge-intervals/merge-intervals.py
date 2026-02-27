ef692e2b876edc7ac5aada3e726194fcde134249
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-02-27 16:26:17 -0600

    Time: 0 ms (100.00%) | Memory: 23.1 MB (35.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,15 +1,24 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sort the intervals based on start times
+        # initialize res
+        # add the first interval to res
+        # iterate the subsequence interval in intervals
+        # if start < previous: merge by getting the earliest sttart and latest end
+        
         if len(intervals) == 1:
             return intervals
         
-        intervals.sort()
+        intervals.sort(key=lambda x: x[0])
         res = [intervals[0]]
-        for curr_start, curr_end in intervals[1:]:
+
+        for start, end in intervals[1:]:
             prev_start, prev_end = res[-1]
-            if prev_end >= curr_start:
-                res[-1][1] = max(curr_end, prev_end)
+
+            if start <= prev_end:
+                new_end = max(prev_end, end)
+                res[-1][1] = new_end
             else:
-                res.append([curr_start, curr_end])
+                res.append([start, end])
         
         return res
