74e8b5911bbaea2c9113e4054366da7e007f7aa8
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-04-21 22:44:12 -0500

    Time: 61 ms (77.42%) | Memory: 22.1 MB (76.24%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,15 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # if either p or q is higher and lower than root, the root is the lCU  
-        # if both p and q are lower than root, then move the root pointer to the left
-        # if both p and q are higher than root, then move the root pointer to the right
-        # if the current root is equal to either p or q, then current root is the lCU
+        # if current node is between p and q, return current node
+        # if current node is bigger than p and q, find common ancestor on root.left 
+        # if current node is smaller than p and q, find common ancestor on root.right  
+        # if current node is p or q, return the current node
 
-        while root:
-            if p.val < root.val and q.val < root.val:
-                root = root.left
-            elif p.val > root.val and q.val > root.val:
-                root = root.right
-            else:
-                return root
+        if not root:
+            return None
+        
+        if root.val == p.val or root.val == q.val:
+            return root
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+        
