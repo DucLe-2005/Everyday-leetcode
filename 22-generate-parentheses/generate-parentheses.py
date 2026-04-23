4c202f6a2baf39a9dc5b5742a4f7c8b39fa477bc
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-04-23 17:21:31 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (39.23%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,16 +2,19 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        def backtrack(s: str = '', openN: int = 0, closeN: int = 0):
+        def dfs(s, openN, closeN):
             if openN == closeN == n:
                 res.append(s)
                 return
-            
+
             if openN < n:
-                backtrack(s + "(", openN + 1, closeN)
+                dfs(s + '(', openN + 1, closeN)
             
             if closeN < openN:
-                backtrack(s + ")", openN, closeN + 1)
-            
-        backtrack()
+                dfs(s + ')', openN, closeN + 1)
+        
+        dfs("", 0, 0)
         return res
+
+        # time O(n * 2^2n) (if there are n pairs, we have 2^2n possible strings, which is an upper bound. the real time complexity is lower. Each string includes a copy operation which takes O(n).
+        # space: O(2n) = O(n)
