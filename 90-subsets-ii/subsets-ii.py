9948a9e8434d12aaaa1730d585e81de029402ace
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-04-23 17:00:15 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (27.58%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,24 +1,23 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         res = []
         cur = []
 
-        nums.sort()
-
-        def backtrack(i):
+        def dfs(i):
             if i == len(nums):
                 res.append(cur[:])
                 return
             
-            # All subsets that include nums[i]
             cur.append(nums[i])
-            backtrack(i + 1)
+            dfs(i+1)
             cur.pop()
 
-            # All subsets that don't include nums[i]
-            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            # skip duplicate numbers at the same recursion level
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
                 i += 1
-            backtrack(i + 1)
 
-        backtrack(0)
+            dfs(i+1)
+        
+        dfs(0)
         return res
