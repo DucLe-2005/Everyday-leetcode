d6ac0849e86351fdc573fcb8aa8105214925da61
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-04-28 19:39:25 -0500

    Time: 248 ms (48.77%) | Memory: 26.9 MB (14.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        visit = set()
-        q = deque()
-        islands = 0
-
-        def bfs():
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            while q:
-                x, y = q.popleft()
-                print((x, y))
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-                    if nx not in range(len(grid)) or ny not in range(len(grid[0])):
-                        continue
-                    if grid[nx][ny] == "1" and (nx, ny) not in visit:
-                        q.append((nx, ny))
-                        visit.add((nx, ny))
-            
+        visited = set()
+        res = 0
+        m, n = len(grid), len(grid[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        q = deque([])
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i, j) not in visit:
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    res += 1
                     q.append((i, j))
-                    visit.add((i, j))
-                    bfs()
-                    islands += 1
-        
-        return islands
+                    visited.add((i, j))
+                    while q:
+                        x, y = q.popleft()
+                        for dx, dy in directions:
+                            nx, ny = x + dx, y + dy
+                            if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == "1":
+                                if (nx, ny) not in visited:
+                                    visited.add((nx, ny))
+                                    q.append((nx, ny))
+    
+        return res
