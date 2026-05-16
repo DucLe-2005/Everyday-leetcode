6656461c6906146775b813c312e8fa10e4c9ad96
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-15 22:45:40 -0500

    Time: 11 ms (84.34%) | Memory: 22.7 MB (39.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list)
+        # use the tuple of a sorted order of the letters as the key to the hash map
+        # aabab: ["babba", "aabbbb"]
+
+        anagrams = defaultdict(list)
         for s in strs:
-            anagram_map[tuple(sorted(list(s)))].append(s)
+            key = tuple(sorted(list(s)))
+            anagrams[key].append(s)
         
-        return [x for x in anagram_map.values()]
+        return [x for x in anagrams.values()]
+    
+        # time: O(c log(c) * n), c is the length of the longest word, n is the number of words
+        # space: O(n)
