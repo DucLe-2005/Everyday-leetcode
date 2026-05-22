8306f94c9bcf7578354b987c55f6ba2e5750b9e7
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-21 21:52:22 -0500

    Time: 1 ms (93.44%) | Memory: 19.1 MB (96.72%) - LeetSync

--- 4029-sort-array-by-absolute-value/sort-array-by-absolute-value.py
+++ 4029-sort-array-by-absolute-value/sort-array-by-absolute-value.py
@@ -1,4 +1,3 @@
 class Solution:
     def sortByAbsoluteValue(self, nums: List[int]) -> List[int]:
-        nums.sort(key=lambda x: x if x >= 0 else -x)
-        return nums
+        return sorted(nums, key=abs)
