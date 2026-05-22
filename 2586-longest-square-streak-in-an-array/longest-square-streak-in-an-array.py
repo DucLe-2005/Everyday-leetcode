afb8cd5b3e144122e8ebca7aa551085e9e52d294
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-21 21:47:47 -0500

    Time: 77 ms (66.67%) | Memory: 64.3 MB (5.26%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -1,24 +1,27 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        # if square root of num is in n, add num: its square root to dictionary
-        # find the longest chain in the dictionary
         # time: O(n)
         # space: O(n)
         nums_set = set(nums)
-        square_roots = {}
-        for num in nums_set:
-            if num ** 2 in nums_set:
-                square_roots[num ** 2] = num
+        memo = {}
 
+        def dfs(x):
+            if x in memo:
+                return memo[x]
+            
+            nxt = x ** 2
+            if nxt not in nums_set:
+                memo[x] = 1
+            else:
+                memo[x] = 1 + dfs(nxt)
+            
+            return memo[x]
+        
         res = -1
-        visited = set()
-        for num in square_roots:
-            visited.add(num)
-            length = 2
-            curr = num
-            while square_roots[curr] in square_roots:
-                curr = square_roots[curr]
-                length += 1
-            res = max(res, length)
+
+        for num in nums_set:
+            length = dfs(num)
+            if length >= 2:
+                res = max(res, length)
         
         return res
