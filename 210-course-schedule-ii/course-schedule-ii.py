e011073613f02f57755921c663a9330aa7c2d802
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-28 03:51:14 -0500

    Time: 0 ms (100.00%) | Memory: 20.3 MB (73.85%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,38 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list) 
-        # key: prerequisite course,  
-        # values: list of courses that need this prereq
-        indegree = [0] * numCourses  # number of prerequisites for each course
+        # time: O(E + V)
+        # space: O(E + V)
+        graph = defaultdict(list)
+        needed_count = [0] * numCourses
 
-        # build the graph and indegree array
-        for child, parent in prerequisites:
-            graph[parent].append(child)
-            indegree[child] += 1
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            needed_count[course] += 1
         
-        # Add course with no prerequisite to queue
-        zero_queue = deque()
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                zero_queue.append(course)
+        q = deque()
+        for i in range(numCourses):
+            if needed_count[i] == 0:
+                q.append(i)  
         
-        # Use BFS to determine the course order
-        course_order = []
-        while zero_queue:
-            course = zero_queue.popleft()
-            course_order.append(course)
-
-            # Decrease the indegree of the course's children by 1
-            for child in graph[course]:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    zero_queue.append(child)
+        order = []
+        while q:
+            item = q.popleft()
+            order.append(item)
+            for course in graph[item]:
+                needed_count[course] -= 1
+                if needed_count[course] == 0:
+                    q.append(course)
         
-        # There is a cycle, so it is impossible to finish all courses
-        if numCourses != len(course_order):
-            return []
-        return course_order
-
-
-        # time: O(V + E)
-        # space: O(V + E)
+        return order if len(order) == numCourses else []
