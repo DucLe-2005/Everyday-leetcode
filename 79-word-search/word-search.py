29f98f8c30d9df682460b9ea75b4a8162e664d10
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-28 09:26:38 -0500

    Time: 4334 ms (32.98%) | Memory: 19.7 MB (8.12%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,47 +1,36 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        n = len(board)
-        m = len(board[0])
-
-        def dfs(i, j, idx):
-            if board[i][j] != word[idx]:
+        # iterate each cell in board
+        # if cell == word[0]: dfs, backtrack if one way return False
+        # if one dfs return True, return True
+        # time: (m^2 * n^2)
+        visiting = set()
+        m, n = len(board), len(board[0])
+        def backtrack(row, col, i):
+            if i == len(word):
+                return True
+            if row < 0 or row == m or col < 0 or col == n:
                 return False
-            if idx == len(word) - 1:
-                return True
+            if (row, col) in visiting:
+                return False
+            if board[row][col] != word[i]:
+                return False
             
-            idx += 1
-            tmp = board[i][j]
-            board[i][j] = '#'
             
-            if i > 0 and dfs(i - 1, j, idx):  # up
-                board[i][j] = tmp
-                return True
-            if i < n - 1 and dfs(i + 1, j, idx):  # down
-                board[i][j] = tmp
-                return True
-            if j > 0 and dfs(i, j - 1, idx):  # left
-                board[i][j] = tmp
-                return True
-            if j < m - 1 and dfs(i, j + 1, idx):
-                board[i][j] = tmp
-                return True
+            visiting.add((row, col))
+            found = (
+                backtrack(row+1, col, i+1) or
+                backtrack(row-1, col, i+1) or
+                backtrack(row, col+1, i+1) or
+                backtrack(row, col-1, i+1)
+            )
+            visiting.remove((row, col))
             
-            # backtrack
-            board[i][j] = tmp
-            return False
+            return found
         
-        # pruning: return immediately if grid's size is smaller than word's length
-        if n * m < len(word):
-            return False
-
-
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == word[0]:
-                    if dfs(i, j, 0):
-                        return True
-        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
         return False
 
-    # time: O(n * m) -> n = grid's size, m = word's length
-    # space: O(1), max recursive stack = O(m)
