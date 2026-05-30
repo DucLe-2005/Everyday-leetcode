006a6c29772db5f051a0baaa42cac4dcadccea30
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-05-30 01:14:39 -0500

    Time: 420 ms (89.16%) | Memory: 47.9 MB (11.45%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
@@ -1,5 +1,7 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        # time: O(m * n), n = len(arr), m = max bit length of any number in arr
+        # space: O(m * n)
         res = set()  
         ORs = set() # ORs values of subarrays that end at arr[i]
         for num in arr:
