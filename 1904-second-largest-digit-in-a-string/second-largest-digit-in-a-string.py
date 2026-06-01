a7077c57c169cbd2d6c8846caf36c4eaacd1888e
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-01 04:45:19 -0500

    Time: 3 ms (74.98%) | Memory: 19.4 MB (8.53%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -3,14 +3,11 @@ class Solution:
         # big1 -> big2
         big1 = big2 = -1
         for c in s:
-            if c in "0123456789":
+            if c.isdigit():
                 c = int(c)
-                if c == big1 or c == big2:
-                    continue
                 if c > big2:
-                    big1 = big2
-                    big2 = c
-                elif c > big1:
+                    big1, big2 = big2, c
+                elif big1 < c < big2:
                     big1 = c
         
         return big1
