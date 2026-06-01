e3b4c97f758c53dc7ccb1958ebe2f8cc5382e864
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-01 04:34:33 -0500

    Time: 40 ms (81.53%) | Memory: 19.3 MB (10.80%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,7 +1,7 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         words = s.split(" ")
-        sentence = words.copy()
+        sentence = [""] * len(words)
 
         for word in words:
             sentence[int(word[-1]) - 1] = word[:-1]
