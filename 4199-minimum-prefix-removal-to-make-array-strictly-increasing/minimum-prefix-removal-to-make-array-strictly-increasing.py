8d21be058f20484de5887ebcafda6647e260be01
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-01 04:39:38 -0500

    Time: 8 ms (83.50%) | Memory: 34.1 MB (87.54%) - LeetSync

--- 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.py
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.py
@@ -1,8 +1,7 @@
 class Solution:
     def minimumPrefixLength(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums) - 1):
-            if nums[i] >= nums[i+1]:
-                res = i + 1
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] <= nums[i-1]:
+                return i
         
-        return res
+        return 0
