a7a64d1b948e468774053b1e387fb18e8e16a520
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-02 08:56:02 -0500

    Time: 67 ms (19.92%) | Memory: 25.5 MB (50.78%) - LeetSync

---
+++ 1165-meeting-scheduler/meeting-scheduler.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        # if start1 > end2: move slot2 forward
+        # if start2 > end1: move slot1 forward
+        # else calculate overlaps
+        slots1.sort()
+        slots2.sort()
+        i, j = 0, 0
+        while i < len(slots1) and j < len(slots2):
+
+            start1, end1 = slots1[i][0], slots1[i][1]
+            start2, end2 = slots2[j][0], slots2[j][1]
+
+            if start1 >= end2:
+                j += 1
+            elif start2 >= end1:
+                i += 1
+            else:
+                end = min(end1, end2)
+                start = max(start1, start2)
+                if end - start >= duration:
+                    return [start, start + duration]
+                elif end1 > end2:
+                    j += 1
+                else:
+                    i += 1
+        
+        return []
