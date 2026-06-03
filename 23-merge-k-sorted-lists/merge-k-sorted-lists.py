c3013751668e485b6d7d01245cbd602cab362a13
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-03 09:01:50 -0500

    Time: 911 ms (11.37%) | Memory: 22.6 MB (93.75%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,34 +5,35 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # time: O(k * n)
         if len(lists) == 0:
             return None
+        if len(lists) == 1:
+            return lists[0]
 
-        interval = 1
-        amount = len(lists)
-        while interval < amount:
-            for i in range(0, amount - interval, interval * 2):
-                lists[i] = self.mergeTwoLists(lists[i], lists[i+interval])
-            interval *= 2
+        res = lists[0]
+        for i in range(1, len(lists)):
+            res = self.merge2Lists(res, lists[i])
         
-        return lists[0]
-        
-    def mergeTwoLists(self, list1, list2):
-        head = ListNode()
-        cur = head
+        return res
+
+    def merge2Lists(self, list1, list2):
+        dummy = ListNode()
+        curr = dummy
         while list1 and list2:
             if list1.val < list2.val:
-                cur.next = list1
-                cur = cur.next
+                curr.next = list1
                 list1 = list1.next
             else:
-                cur.next = list2
-                cur = cur.next
+                curr.next = list2
                 list2 = list2.next
             
-        if list1:
-            cur.next = list1
-        if list2:
-            cur.next = list2
+            curr = curr.next
         
-        return head.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+
+        return dummy.next
+
