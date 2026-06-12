7afa6b908abc435bc1f034a02811379b820b887c
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-12 18:13:00 +0700

    Time: 1 ms (30.40%) | Memory: 19.5 MB (51.88%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        heap = []
+
+        for count, char in [(a, "a"), (b, "b"), (c, "c")]:
+            if count > 0:
+                heapq.heappush(heap, (-count, char))
+        
+        res = []
+
+        while heap:
+            count1, char1 = heapq.heappop(heap)
+
+            if len(res) >= 2 and res[-1] == res[-2] == char1:
+                if not heap:
+                    break
+                
+                count2, char2 = heapq.heappop(heap)  
+                
+                res.append(char2)
+                count2 += 1
+
+                if count2 < 0:
+                    heapq.heappush(heap, (count2, char2))
+                
+                heapq.heappush(heap, (count1, char1))
+            
+            else:
+                res.append(char1)
+                count1 += 1
+            
+                if count1 < 0:
+                    heapq.heappush(heap, (count1, char1))
+            
+        return "".join(res)
