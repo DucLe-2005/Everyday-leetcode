053b7c3e23081b8cc0620f88fba2063ba6e50dd0
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-15 09:31:11 +0700

    Time: 30 ms (92.75%) | Memory: 22.5 MB (84.03%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
@@ -1,25 +1,29 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        # at every index i decide whether to extend subarray or start a new subarray
-        # time: O(n)
-        # space: O(1)
-        if len(arr) == 1:
+        n = len(arr)
+        if n == 1:
             return 1
-        
-        best_arr = curr_arr = 1
-        for i in range(1, len(arr)):
-            if i == 1:
-                curr_arr = 1 if arr[i] == arr[i-1] else 2
-            elif arr[i-2] < arr[i-1] > arr[i]:
-                curr_arr += 1
-            elif i > 1 and arr[i-2] > arr[i-1] < arr[i]:
-                curr_arr += 1
-            elif arr[i] == arr[i-1]:
-                curr_arr = 1
+
+        best = 1
+        curr = 1
+        prev_cmp = 0
+
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                cmp = 1
+            elif arr[i] < arr[i - 1]:
+                cmp = -1
             else:
-                curr_arr = 2
-            
-            print(f"i: {i}, curr_arr: {curr_arr}")
-            best_arr = max(curr_arr, best_arr)
-        
-        return best_arr
+                cmp = 0
+
+            if cmp == 0:
+                curr = 1
+            elif cmp * prev_cmp == -1:
+                curr += 1
+            else:
+                curr = 2
+
+            best = max(best, curr)
+            prev_cmp = cmp
+
+        return best
