c0626b01ee29ce70f893c9293a99c2562a4eba8d
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-19 16:01:14 +0700

    Time: 23 ms (59.93%) | Memory: 26 MB (30.71%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,15 +1,20 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost):
+        # can't travel in circle -> total cost > total gas
+        # if total gas > total cost, find the index where gas > cost as starting point
+        # time: O(n)
+        # space: O(1)
+        if sum(cost) > sum(gas):
             return -1
         
-        start = 0
-        cur = 0
-
+        start_idx = 0
+        gas_left = 0
         for i in range(len(gas)):
-            cur += gas[i] - cost[i]
-            if cur < 0:
-                start = i + 1
-                cur = 0
+            gas_left += gas[i] - cost[i]
+            if gas_left < 0:
+                start_idx = i + 1
+                gas_left = 0
             
-        return start
+        
+        return start_idx
+            
