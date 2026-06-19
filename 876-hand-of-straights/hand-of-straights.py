450015b8c49cb8d651d82cd5f32aa3ee72d47909
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-19 20:58:22 +0700

    Time: 27 ms (88.04%) | Memory: 21.5 MB (10.17%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,22 +1,28 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        count = Counter(hand)
+        # time: O(nlogn)
+        # space: O(n)
 
-        sortedCards = sorted(count.keys())
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = {}
+        for n in hand:
+            count[n] = count.get(n, 0) + 1
+        
+        min_list = [key for key in count.keys()]
+        heapq.heapify(min_list)
 
-        groupStartQueue = deque()
-        openGroups = 0
-        lastCard = -1
+        while min_list:
+            first = min_list[0]
 
-        for currentCard in sortedCards:
-            if (openGroups > 0 and currentCard > lastCard + 1) or openGroups > count[currentCard]:
-                return False
-            
-            groupStartQueue.append(count[currentCard] - openGroups)
-            lastCard = currentCard
-            openGroups = count[currentCard]
-
-            if len(groupStartQueue) == groupSize:
-                openGroups -= groupStartQueue.popleft()
-            
-        return openGroups == 0
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != min_list[0]: # there's a hole
+                        return False
+                    heapq.heappop(min_list)
+        
+        return True
