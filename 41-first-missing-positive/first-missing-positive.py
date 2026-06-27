547ffd5825ec7b6ca0bee4d6dabd5541c7621971
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-06-27 21:21:24 +0700

    Time: 58 ms (29.02%) | Memory: 31 MB (37.53%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,19 +1,23 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # 1. cycle sort nums
+        # if num in [1,n]: put them in index (num - 1)
+        # else: skip
+        # the firt number that val != index + 1 is the missing smallest positive integer
+        
+        # cycle sort nums
         n = len(nums)
         i = 0
-        # Cycle sort numbers from 1 to n
         while i < n:
             correct_idx = nums[i] - 1
-            if 0 < nums[i] <= n and nums[correct_idx] != nums[i]:
-                nums[correct_idx], nums[i] = nums[i], nums[correct_idx]
+            if 0 <= correct_idx < n and nums[correct_idx] != nums[i]:
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
             else:
                 i += 1
-        # Return the first missing positive number in the sorted array        
+        
+        # find the first number that is not in the right order
         for i in range(n):
             if nums[i] != i + 1:
                 return i + 1
         
-        return n + 1  # If 1 to n are present, n + 1 is the first missing positive number
-        
-        
+        return n + 1
