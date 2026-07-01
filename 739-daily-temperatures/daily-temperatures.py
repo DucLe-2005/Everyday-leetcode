f134a760ab68a73ca0b1b9f4b00a19e2b18c4a95
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-01 16:14:04 +0700

    Time: 119 ms (24.05%) | Memory: 36.6 MB (5.88%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,19 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # initialize answers = [0] * len(temperatures)
-        # declare a stack that is monotonically decreasing
-        # traverse temperatures
-        # if temp is lower than top of the stack, add to stack
-        # if temp is higher than top of the stack, pop the stack, set answers[i] = temperatures[i] - stack.pop()
         # time: O(n)
         # space: O(n)
-
+        res = [0] * len(temperatures)
         stack = []
-        answer = [0 for _ in range(len(temperatures))]
 
         for i, temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temp:
-                prev_i = stack.pop()
-                answer[prev_i] = i - prev_i
-            stack.append(i)
-        return answer
+            while stack and temp > stack[-1][1]:
+                prev_i, prev_temp = stack.pop()  
+                res[prev_i] = i - prev_i
+            stack.append([i, temp])
+        
+        return res
