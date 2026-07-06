3d15960ad7979c4b52ce452b7ffeed9157a20d3f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-06 12:13:09 +0700

    Time: 173 ms (58.82%) | Memory: 66.5 MB (72.61%) - LeetSync

---
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.py
@@ -0,0 +1,43 @@
+class StockPrice:
+
+    def __init__(self):
+        # (p1, t1) -> (p2, t2) -> (p3, t3)
+        self.records = defaultdict(int) # timestamp = price
+        self.min_records = [] # min heap
+        self.max_records = [] # max heap
+        self.latest_timestamp = -1
+
+    def update(self, timestamp: int, price: int) -> None:
+        self.records[timestamp] = price
+        heapq.heappush(self.min_records, (price, timestamp))
+        heapq.heappush(self.max_records, (-price, timestamp))
+        self.latest_timestamp = max(timestamp, self.latest_timestamp)
+
+    def current(self) -> int:
+        return self.records[self.latest_timestamp]
+
+    def maximum(self) -> int:
+        while self.max_records:
+            price, timestamp = self.max_records[0]
+
+            if self.records[timestamp] == -price:
+                return -price
+
+            heapq.heappop(self.max_records)
+
+    def minimum(self) -> int:
+        while self.min_records:
+            price, timestamp = self.min_records[0]
+
+            if self.records[timestamp] == price:
+                return price
+
+            heapq.heappop(self.min_records)
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
