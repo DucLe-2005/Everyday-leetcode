c0a1439945ae87b72fbd15d3dbfe3ffcdd624efb
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-09 16:22:01 +0700

    Time: 2 ms (5.09%) | Memory: 19.4 MB (39.33%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,28 +1,47 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # if nums[l] <= nums[r]: sorted window
+        # move l = m + 1 if nums[m] < target else r = m - 1
+
+
+        # unsorted window
+        # if m on left and m < target: l = m + 1
+        # if m on left and m > target and target > l: r - m - 1
+        # if m on left and m > target and target < l: l = m + 1
+
+        # if m on right and m > target: r = m - 1
+        # if m on right and m < target and target < r: l = m + 1
+        # if m on right and m < target and target > r: r = m - 1
+        
+
         l, r = 0, len(nums) - 1
+        i = 1
         while l <= r:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-            if nums[l] <= nums[m]:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
             
-            else:
-                if nums[m] < target <= nums[r]:
+            # sorted window
+            if nums[l] < nums[r]:
+                if nums[m] < target:
                     l = m + 1
                 else:
                     r = m - 1
-
-        return -1
-
                 
-            
-
-            # [4, 5, 6, 7]
-            # target = 6
-            # middle = 5
-            
+            # unsorted window
+            else:
+                # m is on left portion
+                if nums[m] >= nums[l]:
+                    if nums[m] < target or target < nums[l]:
+                        l = m + 1
+                    else:
+                        r = m - 1
+                # m is on right portion
+                else:
+                    if nums[m] > target or target > nums[r]:
+                        r = m - 1
+                    else:
+                        l = m + 1
+            i += 1
+        
+        return -1
