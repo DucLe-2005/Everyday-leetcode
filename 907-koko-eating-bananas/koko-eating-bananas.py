1b2fb577f341750d595c555badc48f7952ead091
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-09 11:24:36 +0700

    Time: 175 ms (41.26%) | Memory: 20.7 MB (41.35%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,18 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+        # time: O(n * log(k)), k = max(piles), n = len(piles)
+        low, high = 1, max(piles)
+        res = high
+        while low <= high:
+            mid = (low + high) // 2
 
-        while l <= r:
-            hours = 0
-            mid = (l + r) // 2
-            for pile in piles:
-                hours += math.ceil(pile / mid)
+            time = 0
+            for p in piles:
+                time += math.ceil(p / mid)
             
-            if hours <= h:
-                res = min(res, mid)
-                r = mid - 1
+            if time <= h:
+                high = mid - 1
+                res = mid
             else:
-                l = mid + 1
+                low = mid + 1
         
         return res
