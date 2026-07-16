b2dfa8f5f37d63d8c5410b7d15a8910bd186a2e1
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-16 16:07:06 +0700

    Time: 52 ms (98.11%) | Memory: 22.8 MB (44.00%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,25 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # if root is p or q and found p or q on either left or right child: root is lca
-        # if root is not p or q and found p or q on both or right child: root is lca
-        res = None
-
-        def dfs(node):
-            nonlocal res
-            if not node or res:
-                return 0
-
-            left = dfs(node.left)
-            right = dfs(node.right)
-            x = left + right
-            if node.val == p.val or node.val == q.val:
-                x += 1
-            
-            if x == 2 and not res:
-                res = node
-
-            return x
+        if not root:
+            return None
         
-        dfs(root)
-        return res
+        if root.val == p.val or root.val == q.val:
+            return root
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
