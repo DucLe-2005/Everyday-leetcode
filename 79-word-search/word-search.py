78f179cb2b4bdc05ebc94b3cc8b9baa525367e38
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-22 08:15:10 -0500

    Time: 628 ms (94.37%) | Memory: 19.8 MB (7.87%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,37 +1,54 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # iterate each cell in board
-        # if cell == word[0]: dfs, backtrack if one way return False
-        # if one dfs return True, return True
-        # time: O(m * n * 3^L)
+        # time: O(mn * 3^L)
         # space: O(L)
-        visiting = set()
-        m, n = len(board), len(board[0])
-        def backtrack(row, col, i):
+        rows = len(board)
+        cols = len(board[0])
+
+        # not enough cells
+        if len(word) > rows * cols:
+            return False
+        
+        # not enough required characters
+        board_count = Counter(
+            board[r][c]
+            for r in range(rows)
+            for c in range(cols)
+        )
+
+        word_count = Counter(word)
+        for char, needed in word_count.items():
+            if board_count[char] < needed:
+                return False
+            
+    
+        visited = set()
+        def check(i: int, r: int, c: int) -> bool:
             if i == len(word):
                 return True
-            if row < 0 or row == m or col < 0 or col == n:
+            if (
+                r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                (r, c) in visited or
+                board[r][c] != word[i]
+            ):
                 return False
-            if (row, col) in visiting:
-                return False
-            if board[row][col] != word[i]:
-                return False
-            
-            
-            visiting.add((row, col))
-            found = (
-                backtrack(row+1, col, i+1) or
-                backtrack(row-1, col, i+1) or
-                backtrack(row, col+1, i+1) or
-                backtrack(row, col-1, i+1)
-            )
-            visiting.remove((row, col))
-            
-            return found
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtrack(i, j, 0):
-                    return True
-        return False
 
+            visited.add((r, c))
+
+            found = (
+                check(i + 1, r + 1, c) or
+                check(i + 1, r - 1, c) or
+                check(i + 1, r, c + 1) or
+                check(i + 1, r, c - 1)
+            )
+
+            visited.remove((r, c))
+            return found
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0] and check(0, i, j):
+                    return True
+
+        return False
