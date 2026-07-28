1ea28d82d027fcc02e7684a30e138da01a17e244
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-28 00:05:43 -0500

    Time: 0 ms (100.00%) | Memory: 20.9 MB (42.03%) - LeetSync

---
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        # if root.val == target: return target
+        # if root.val < target: dfs(root.right, target)
+        # if root.val > target: dfs(root.left, target)
+
+        # if abs(root.val - target) < res: res = root.val
+
+
+        res = root.val
+        min_diff = abs(root.val - target)
+        def dfs(node):
+            nonlocal res, min_diff
+            if not node:
+                return
+            
+            diff = abs(node.val - target)
+            if diff == min_diff:
+                res = min(node.val, res)
+            elif diff < min_diff:
+                res = node.val
+                min_diff = diff
+
+            if node.val < target:
+                dfs(node.right)
+            else:
+                dfs(node.left)
+        
+        dfs(root)
+        return res
