939880daa105d0e37efb303462d76a359dc2599e
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-07-28 23:52:44 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (18.87%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,37 +1,37 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        # time: O(a + b + c)
+        # time: O(n) (On log(3))
         # space: O(1)
         heap = []
 
-        for count, char in [(a, "a"), (b, "b"), (c, "c")]:
-            if count > 0:
-                heapq.heappush(heap, (-count, char))
-        
+        for char, frequency in [("a", a), ("b", b), ("c", c)]:
+            if frequency > 0:
+                heapq.heappush(heap, (-frequency, char))
         res = []
-
+        
         while heap:
-            count1, char1 = heapq.heappop(heap)
-
-            if len(res) >= 2 and res[-1] == res[-2] == char1:
+            curr_count, curr_char = heapq.heappop(heap)
+            
+            if len(res) >= 2 and res[-1] == res[-2] == curr_char:
+                # a letter appears 3 times in a row
                 if not heap:
                     break
                 
-                count2, char2 = heapq.heappop(heap)  
-                
-                res.append(char2)
-                count2 += 1
+                # Get the next most frequent char
+                prev_char, prev_count = curr_char, curr_count
+                curr_count, curr_char = heapq.heappop(heap)
 
-                if count2 < 0:
-                    heapq.heappush(heap, (count2, char2))
+                res.append(curr_char)
+                curr_count += 1
                 
-                heapq.heappush(heap, (count1, char1))
-            
-            else:
-                res.append(char1)
-                count1 += 1
-            
-                if count1 < 0:
-                    heapq.heappush(heap, (count1, char1))
-            
+                # Push 2 previously popped char back to heap
+                if curr_count < 0:
+                    heapq.heappush(heap, (curr_count, curr_char))
+                heapq.heappush(heap, (prev_count, prev_char))
+            else: # Add most frequent char to res
+                res.append(curr_char)
+                curr_count += 1
+                if curr_count < 0:
+                    heapq.heappush(heap, (curr_count, curr_char))
+    
         return "".join(res)
