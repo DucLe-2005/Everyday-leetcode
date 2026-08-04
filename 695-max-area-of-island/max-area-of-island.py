f9fbd6764703cc3045a176fb1696c27e438f8cbc
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-03 23:33:23 -0500

    Time: 23 ms (39.33%) | Memory: 19.6 MB (84.66%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,22 +1,38 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res = 0
-        rows, cols = len(grid), len(grid[0])
+        # time: O(m * n)
+        # space: O(m * n)
+        m, n = len(grid), len(grid[0])
+        visited = set()
 
-        def dfs(r, c):
-            if not (r in range(rows) and c in range(cols) and grid[r][c] == 1):
-                return 0
+        def bfs(start_r: int, start_c: int):
+            queue = deque([(start_r, start_c)])
+            area = 0
 
-            grid[r][c] = 0  # mark this position as visited
+            while queue:
+                r, c = queue.popleft()
+                visited.add((r, c))
+                area += 1
+                for dr, dc in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                    nr, nc = r + dr, c + dc
 
-            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    res = max(res, dfs(r, c))
+                    if (
+                        0 <= nr < m and
+                        0 <= nc < n and
+                        grid[nr][nc] == 1 and 
+                        (nr, nc) not in visited
+                    ):
+                        visited.add((nr, nc))
+                        queue.append((nr, nc))
+                    
+            return area
         
-        return res
+        max_area = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    max_area = max(max_area, bfs(r, c))
         
-    # time: O(m *  n)
-    # space: O(m * n)
+        return max_area
+                
+        
