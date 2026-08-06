286560f667e284d4d382b818abc7c8b01042a859
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-05 21:59:11 -0500

    Time: 1 ms (88.63%) | Memory: 22.7 MB (20.01%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,40 +3,40 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # check all sides of the board
-        # add all 'O' to a queue
-        # use dfs on on all 'O' in the queues to find regions that are not surrounded, mark those positions as open
-        # iterate each position on the board to translate 'O' to 'X', except those that are marked as 'open'
-
-        # time: O(m * m)
+        # time: O(m * n)
         # space: O(m * n)
-    
         m, n = len(board), len(board[0])
-        isSurrounded = [[True] * n for _ in range(m)]
 
-        def dfs(i, j):
-            if i not in range(m) or j not in range(n) or board[i][j] != 'O' or not isSurrounded[i][j]:
+        def dfs(r: int, c: int) -> bool:
+            if (
+                r < 0 or r >= m or
+                c < 0 or c >= n or
+                board[r][c] != 'O'
+            ):
                 return
-            isSurrounded[i][j] = False
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1) 
 
-        for i in range(m):
-            if board[i][0] == 'O':
-                dfs(i, 0)
-            if board[i][n - 1] == 'O':
-                dfs(i, n-1)
+            board[r][c] = 'T'
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        # Mark all 'O' connected to left and right borders
+        for row in range(m):
+            dfs(row, 0)
+            dfs(row, n - 1)
         
-        for j in range(n):
-            if board[0][j] == 'O':
-                dfs(0, j)
-            if board[m-1][j] == 'O':
-                dfs(m-1, j)
+        # Mark all 'O' connected to top and bottom borders
+        for col in range(n):
+            dfs(0, col)
+            dfs(m - 1, col)
 
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == 'O' and isSurrounded[r][c]:
-                    board[r][c] = 'X'
+        # Convert all 'O' to 'X', 'T' to 'O'
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'T':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
         
