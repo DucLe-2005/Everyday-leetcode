c7081ff0d6af6c16ad2e6c97faf6254119d47e67
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-06 21:44:45 -0500

    Time: 4 ms (48.16%) | Memory: 20.4 MB (55.01%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,26 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # time: O(E + V)
-        # space: O(E + V)
-        graph = defaultdict(list)
-        needed_count = [0] * numCourses
+        # time: O(V + E)
+        # space: O(V + E)
+        prereq = defaultdict(list)
+        need_count = [0] * numCourses
+        for a, b in prerequisites:
+            prereq[b].append(a)
+            need_count[a] += 1
+        
+        # take courses whose prerequisites are satisfied
+        q = deque([])
+        for c in range(numCourses):
+            if need_count[c] == 0:
+                q.append(c)
 
-        for course, prereq in prerequisites:
-            graph[prereq].append(course)
-            needed_count[course] += 1
-        
-        q = deque()
-        for i in range(numCourses):
-            if needed_count[i] == 0:
-                q.append(i)  
-        
         order = []
         while q:
-            item = q.popleft()
-            order.append(item)
-            for course in graph[item]:
-                needed_count[course] -= 1
-                if needed_count[course] == 0:
+            prereq_course = q.popleft()
+            order.append(prereq_course)
+            for course in prereq[prereq_course]:
+                need_count[course] -= 1
+
+                if need_count[course] == 0:
                     q.append(course)
-        
+
         return order if len(order) == numCourses else []
