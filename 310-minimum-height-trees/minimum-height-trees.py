f7e1bb19c1872b32efe5d509d7c08a2be16197d8
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-11 05:16:34 -0500

    Time: 40 ms (81.93%) | Memory: 27.6 MB (95.68%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,31 +1,37 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        # time: O(n)
+        # space: O(n)
+
         if n == 1:
             return [0]
 
-        adj = defaultdict(list)
-        for x, y in edges:
-            adj[x].append(y)
-            adj[y].append(x)
-        
-        edge_cnt = {}
-        leaves = deque()
-        for node, neighbors in adj.items():
-            if len(neighbors) == 1:
-                leaves.append(node)
-            edge_cnt[node] = len(neighbors)
-        
-        while True:
-            if n <= 2:
-                return list(leaves)
-            
-            for i in range(len(leaves)):
-                node = leaves.popleft()
-                n -= 1
-                for nei in adj[node]:
-                    edge_cnt[nei] -= 1
-                    if edge_cnt[nei] == 1:
-                        leaves.append(nei)
-            
+        graph = defaultdict(list)
+        degree = [0] * n
 
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            degree[a] += 1
+            degree[b] += 1
         
+        q = deque([])
+        for i in range(n):
+            if degree[i] == 1:
+                q.append(i)
+
+        remaining = n
+        while remaining > 2:
+            leaf_count = len(q)
+            remaining -= leaf_count
+
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                for nei in graph[node]:
+                    degree[nei] -= 1
+
+                    if degree[nei] == 1:
+                        q.append(nei)
+        
+        return list(q)
