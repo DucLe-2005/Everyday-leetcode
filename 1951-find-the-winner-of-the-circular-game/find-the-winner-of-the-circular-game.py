210acec7c873ff6f5d5313962afeea59f589488f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-20 15:54:49 -0500

    Time: 4 ms (40.52%) | Memory: 19.5 MB (22.91%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,18 +1,16 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # queue: [1, ...., n]
-        # while queue has more than 1 element:
-        # for k - 1 times:
-        # pop from queue and push to queue
-        # the first node in queue is eliminated
-        # time: O(n * k)
-        # space: O(n)
+        # time: O(n^2)
+        # space: O(1)
+        s = [x for x in range(1, n+1)]
+        l = n
+        i = 0
+        k -= 1
+        while l > 1:
+            i += k
+            i %= l
+            del s[i]
+            l -= 1
+        return s[0]
 
-        q = deque([x for x in range(1, n+1)])
-        while len(q) > 1:
-            for _ in range(k - 1):
-                q.append(q.popleft())
-            q.popleft()
-        
-        return q[0]
 
