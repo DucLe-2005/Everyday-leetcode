8aa50081343bb4d3c4cde47ec0b714d0c5526b90
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-20 15:37:19 -0500

    Time: 58 ms (56.51%) | Memory: 29.6 MB (67.44%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,11 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        # use two pointers with left and right being the two sides of the container
+        # update the volume of the container
+        # move the pointer of lower height
+        
         res = 0
         l, r = 0, len(height) - 1
         while l < r:
-            res = max(res, (r - l) * min(height[l], height[r]))
-
-            if height[l] >= height[r]:
+            volume = min(height[r], height[l]) * (r - l)
+            res = max(res, volume)
+            if height[l] > height[r]:
                 r -= 1
             else:
                 l += 1
