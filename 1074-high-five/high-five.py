568a3a1f58456845766267641987eac73ff16ffb
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-20 15:33:03 -0500

    Time: 4 ms (14.32%) | Memory: 19.8 MB (12.65%) - LeetSync

--- 1074-high-five/high-five.py
+++ 1074-high-five/high-five.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        # time: O(n + i log i)
+        # space: O(i)
+        # n = number of items
+        # i = number of unique student IDs
         scores = defaultdict(list)
-        for i, score in items:
-            scores[i].append(score)
+        for student_id, score in items:
+            heapq.heappush(scores[student_id], score)
+            if len(scores[student_id]) > 5:
+                heapq.heappop(scores[student_id])
         
-        res = []
-        for x in sorted(scores.keys()):
-            scores[x].sort(reverse=True)
-            size = len(scores[x])
-            top_five = scores[x][:5] if size > 5 else scores[x][:size]
-            res.append([x, sum(top_five) // 5])
-        
-        return res
+        return sorted([[student_id, sum(top_scores) // 5] for student_id, top_scores in scores.items()])
