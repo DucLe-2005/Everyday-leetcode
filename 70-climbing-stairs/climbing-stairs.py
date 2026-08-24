e3f1ebbd379b94f3c92f0971a68dfc0ef206ce42
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-24 16:38:10 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.08%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,22 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # DP
-        # Reasoning: 
-        # At stair i, to get to the top, there are n - i steps.
-        # the number of ways that I can climb to the top is the sume of:
-        # 1. the number of ways that I can climb to the top from stair n + 1
-        # 2. the number of ways that I can climb to the top from stair n + 2
-        # At the top (stair n), there is only 1 way for me to get to the top   
-        # At stair n - 1, there is only 1 way for me to get to the top (by climb 1 step)
-        # dp[i] = dp[i+1] + dp[i+2]
-        # time: O(n), space: O(1)
+        # climb1 -> climb2 -> ...
 
-        two_steps = 1
-        one_step = 1
-        for i in range(n - 2, -1, -1):
-            curr = two_steps + one_step
-            two_steps = one_step
-            one_step = curr
+        climb1, climb2 = 0, 1
+        for _ in range(n):
+            new_climb = climb1 + climb2
+            climb1 = climb2
+            climb2 = new_climb
         
-        return one_step
-        
+        return climb2
