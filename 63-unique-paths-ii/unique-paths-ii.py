de18b6af3340d64611b493c70d0627babeabcb86
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-26 15:44:43 -0500

    Time: 2 ms (24.82%) | Memory: 19.4 MB (31.33%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,24 +1,18 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        if obstacleGrid[0][0] == 1 or obstacleGrid[m - 1][n - 1] == 1:
-            return 0
-        
-        def dfs(row, col):
-            if row == 0 and col == 0:
-                return 1
-            if (row, col) in memo:
-                return memo[(row, col)]
-            
-            paths = 0
-            if row > 0 and obstacleGrid[row - 1][col] == 0:
-                paths += dfs(row - 1, col)
-            if col > 0 and obstacleGrid[row][col - 1] == 0:
-                paths += dfs(row, col - 1)
 
-            memo[(row, col)] = paths
-            return paths
-        
-        memo = {}
-        return dfs(m - 1, n - 1)
+        dp = [[0] * n for _ in range(m)]
+        dp[m-1][n-1] = 1
 
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    if i + 1 < m:
+                        dp[i][j] += dp[i+1][j]
+                    if j + 1 < n:
+                        dp[i][j] += dp[i][j+1]
+        
+        return dp[0][0]
