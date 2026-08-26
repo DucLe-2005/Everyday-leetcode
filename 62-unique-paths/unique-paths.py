5469e0286040ea79b58a15696b9c8b6b2991b472
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-26 15:21:53 -0500

    Time: 4 ms (6.28%) | Memory: 19.3 MB (43.02%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,18 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # create a 2D array with dimensions m x n
-        # let the last row of the 2D array be all 1
-        # this is because there is only one way for each at the last row to reach the corner by going right  
-        # iterate right to left, bottom to top
-        # the number of ways to get the corner at each position equals to the sum
-        # of the number of ways to get to the corner starting at the position to the right and position below
-        # the answer will be the first number in the first array of the 2D array
-        # time: O(m * n), space: O(m * n)
+        # all the cells at the bottom have 1 way to reach star
+        # all the cells at the rght side have 1 way to reach star
+        # dp[i][j] += dp[i+1][j] + dp[i][j+1]
+        # time: O(m+n)
+        # space: O(m+n)
 
-        path_sums = [[1 for _ in range(n)] for i in range(m)]
-
+        dp = [[1] * n for _ in range(m)]
+        
         for i in range(m - 2, -1, -1):
             for j in range(n - 2, -1, -1):
-                path_sums[i][j] = path_sums[i + 1][j] + path_sums[i][j + 1]
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
         
-        return path_sums[0][0]
+        return dp[0][0]
