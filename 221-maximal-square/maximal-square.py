32206f05a5cefe5fb92dc547608f8c24cd55777b
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-27 11:37:23 -0500

    Time: 85 ms (52.26%) | Memory: 33.1 MB (48.99%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,27 +1,10 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        if m == 1 and n == 1 and matrix[0][0] == "0":
-            return 0
-        
-        max_side = 0
-        dp = [] # dp[i][j]: largest square seen in the matrix that ends here
-        for row in matrix:
-            new_row = []
-            for column in row:
-                if column == "1":
-                    max_side = 1
-                new_row.append(int(column))
-            dp.append(new_row)
-        
-        
+        dp = [[int(item) for item in row] for row in matrix]
         for i in range(1, m):
             for j in range(1, n):
                 if dp[i][j] == 1:
-                    if dp[i-1][j-1] == 0 or dp[i-1][j] == 0 or dp[i][j-1] == 0:
-                        max_side = max(max_side, 1)
-                        continue
-                    dp[i][j] += min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
-                    max_side = max(max_side, dp[i][j])
+                    dp[i][j] += min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
 
-        return max_side * max_side
+        return max([max(row) for row in dp]) ** 2
