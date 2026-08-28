fba0710233ac32930f390db879aa708aecea6c8f
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-08-28 18:50:05 -0500

    Time: 27 ms (94.53%) | Memory: 22.6 MB (80.09%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,29 +1,28 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        self.memo = [[None] * (n + 1) for _ in range(m + 1)]
-        return self.minDistanceRecur(word1, word2, len(word1), len(word2))
-    
-    def minDistanceRecur(self, word1, word2, index1, index2):
-        if index1 == 0:
-            return index2
-        if index2 == 0:
-            return index1
-        
-        if self.memo[index1][index2]:
-            return self.memo[index1][index2]
-        
-        minDistance = 0
-        if word1[index1 - 1] == word2[index2 - 1]:
-            minDistance = self.minDistanceRecur(word1, word2, index1 - 1, index2 - 1) # skip char
-        else:
-            replace = self.minDistanceRecur(word1, word2, index1 - 1, index2 - 1)
-            delete = self.minDistanceRecur(word1, word2, index1 - 1, index2)
-            insert = self.minDistanceRecur(word1, word2, index1, index2 - 1)
-            minDistance = min(replace, delete, insert) + 1
-        
-        self.memo[index1][index2] = minDistance
-        return minDistance
-    
-    # time: O(m * n), m = len(word1), n = len(word2)
-    # space: O(m * n), m = len(word1), n = len(word2)
+        memo = [[None for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
+
+        def dfs(word1Index, word2Index):
+            if word1Index == 0:
+                return word2Index
+            if word2Index == 0:
+                return word1Index
+            if memo[word1Index][word2Index]:
+                return memo[word1Index][word2Index]
+            
+            
+            if word1[word1Index-1] == word2[word2Index-1]:
+                distance = dfs(word1Index - 1, word2Index - 1)
+            else:
+                distance = 1 + min(
+                    dfs(word1Index - 1, word2Index - 1),
+                    dfs(word1Index, word2Index - 1),
+                    dfs(word1Index - 1, word2Index)
+                )
+            
+            memo[word1Index][word2Index] = distance
+
+            return distance
+
+        return dfs(len(word1), len(word2))
+
