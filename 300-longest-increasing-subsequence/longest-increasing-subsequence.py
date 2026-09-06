cbf8f83a9674cc85cfdcc568e32662e897cadaed
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-05 19:45:02 -0500

    Time: 11 ms (79.38%) | Memory: 19.6 MB (17.52%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,28 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # Reasoning: sub[i] stores the minimum possible tail of an increasing subsequence of length i + 1. Appending increases the best-known length only when we can extend an existing subsequence. Replacing improves tails withouth changing the best-known length. Therefore, the length of sub at the end equals the LIS length.
+        # DP
+        # time: O(n^2)
+        # space: O(n)
+        # dp = [1] * (len(nums) + 1)
+        # for i in range(1, len(nums) + 1):
+        #     for j in range(i):
+        #         if nums[i - 1] > nums[j - 1]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        
+        # return dp[-1]
+
+        # greedy
+        # time: O(n^2)
+        # space: O(n)
         sub = [nums[0]]
 
-        # find the index of the smallest element that is greater
-        # than or equal to num
-        def find_index(sub, num):
-            l, r = 0, len(sub)
-            while l < r:
-                m = (l + r) // 2
-                if sub[m] >= num:
-                    r = m
-                else:
-                    l = m + 1
-
-            return l   
-
-        for num in nums[1:]:
-            i = find_index(sub, num)
-
-            if i == len(sub):
+        for num in nums:
+            if num > sub[-1]:
                 sub.append(num)
             else:
+                i = 0
+                while num > sub[i]:
+                    i += 1
                 sub[i] = num
         
         return len(sub)
-        
