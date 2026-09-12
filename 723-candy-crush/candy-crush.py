d2192cf5b7ba8b0a6705f0ca7b8bef3aa9ace19b
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-12 18:38:53 -0500

    Time: 48 ms (44.89%) | Memory: 19.5 MB (46.57%) - LeetSync

--- 723-candy-crush/candy-crush.py
+++ 723-candy-crush/candy-crush.py
@@ -1,45 +1,50 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         m, n = len(board), len(board[0])
-        while True:
-        # Mark all cells to crush
-            crush = [[False] * n for _ in range(m)]
-            has_crush = False
 
-            # Horizontal
+        while True:
+            crushed = True
+
             for i in range(m):
                 for j in range(n - 2):
-                    if board[i][j] != 0 and board[i][j] == board[i][j+1] == board[i][j+2]:
-                        crush[i][j] = crush[i][j+1] = crush[i][j+2] = True
-                        has_crush = True
+                    if board[i][j] == 0:
+                        continue
 
-            # Vertical
+                    value = abs(board[i][j])
+
+                    if value == abs(board[i][j+1]) == abs(board[i][j+2]):
+                        board[i][j] = -value
+                        board[i][j+1] = -value
+                        board[i][j+2] = -value
+                        crushed = False
+
             for j in range(n):
                 for i in range(m - 2):
-                    if board[i][j] != 0 and board[i][j] == board[i+1][j] == board[i+2][j]:
-                        crush[i][j] = crush[i+1][j] = crush[i+2][j] = True
-                        has_crush = True
-            
-            if not has_crush:
-                break
-            
-            # Crush marked cells
-            for i in range(m):
-                for j in range(n):
-                    if crush[i][j]:
-                        board[i][j] = 0
-            
-            # Drop candies
-            for j in range(n):
-                write_index = m - 1
+                    if board[i][j] == 0:
+                        continue
+                    
+                    value = abs(board[i][j])
 
-                for i in range(m - 1, -1, -1):
-                    if board[i][j] != 0:
-                        board[write_index][j] = board[i][j]
-                        write_index -= 1
+                    if value == abs(board[i+1][j]) == abs(board[i+2][j]):
+                        board[i][j] = -value
+                        board[i+1][j] = -value
+                        board[i+2][j] = -value
+                        crushed = False
+            
+            if crushed:
+                return board
+            
+            for c in range(n):
+                write = m - 1
 
-                while write_index >= 0:
-                    board[write_index][j] = 0
-                    write_index -= 1
-        
-        return board
+                for r in range(m - 1, -1, -1):
+                    if board[r][c] > 0:
+                        board[write][c] = board[r][c]
+                        write -= 1
+
+                while write >= 0:
+                    board[write][c] = 0
+                    write -= 1
+ 
+
+
