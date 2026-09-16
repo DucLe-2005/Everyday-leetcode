288db6795485c07d865f58f9f2155da2e9167d63
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-16 11:52:49 -0500

    Time: 74 ms (60.00%) | Memory: 66.2 MB (30.69%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,19 +1,24 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        balls, colors = defaultdict(int), defaultdict(int)  # ball : color; color : count
-        res, distinct = [], 0
-        for ball, color in queries:
-            if ball in balls:
-                colors[balls[ball]] -= 1
-                if colors[balls[ball]] == 0:
-                    distinct -= 1
-            
-            balls[ball] = color
-            colors[color] += 1
-            
-            if colors[color] == 1:  # if a new color is added, one more distinct color found
-                distinct += 1
+        # ball_colors: ball -> color
+        # color_count: color -> count
+        # if color_count[color] == 0: total_colors -= 1
 
-            res.append(distinct)
+        ball_colors = {}
+        color_count = {}
+        res = []
 
+        for ball, new_color in queries:
+            prev_color = ball_colors.get(ball, -1)
+            
+            if prev_color in color_count:
+                color_count[prev_color] -= 1        
+                if color_count[prev_color] == 0:
+                    del color_count[prev_color]
+            
+            ball_colors[ball] = new_color
+            color_count[new_color] = color_count.get(new_color, 0) + 1
+
+            res.append(len(color_count))
+        
         return res
