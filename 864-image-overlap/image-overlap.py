27d9815b054b27cf5341787561b02f40ca453d5b
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-15 22:27:07 -0500

    Time: 188 ms (62.41%) | Memory: 19.9 MB (9.97%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -1,32 +1,25 @@
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         n = len(img1)
-
-        # Record the coordinates of 1's in img1
-        A = []
-        for r in range(n):
-            for c in range(n):
-                if img1[r][c] == 1:
-                    A.append((r, c))
         
-        # Record the coordinates of 1's in img2
-        B = []
-        for r in range(n):
-            for c in range(n):
-                if img2[r][c] == 1:
-                    B.append((r, c))
+        ones1 = []
+        for i in range(n):
+            for j in range(n):
+                if img1[i][j] == 1:
+                    ones1.append([i, j])
         
-        # If either image has no 1's, there is no overlap
-        if not A or not B:
+        ones2 = []
+        for i in range(n):
+            for j in range(n):
+                if img2[i][j] == 1:
+                    ones2.append([i, j])
+        
+        if not ones1 or not ones2:
             return 0
+
+        shifts = defaultdict(int)
+        for a, b in ones1:
+            for c, d in ones2:
+                shifts[(a - c, b - d)] += 1
         
-        # Use a hash table to count occurences of translation vectors
-        d = collections.defaultdict(int)
-        for ax, ay in A:
-            for bx, by in B:
-                x_diff = bx - ax
-                y_diff = by - ay
-                d[(x_diff, y_diff)] += 1
-        
-        # The largest value in the hash table is the maximum overlap
-        return max(d.values())
+        return max(shifts.values())
