cf7dae9a972f9f37e4e92e9f12633ed78412b643
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-19 18:14:40 -0500

    Time: 703 ms (5.00%) | Memory: 31.1 MB (31.62%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,11 +1,29 @@
+def max_heapify(a, i, n):
+    while 2*i + 1 < n:
+        left, right = 2*i + 1, 2*i + 2
+        
+        child = left
+        if right < n and a[right] > a[left]: 
+            child = right
+        if a[i] >= a[child]: 
+            return
+        
+        a[i], a[child] = a[child], a[i]
+        i = child
+
+def build_max_heap(a):
+    n = len(a)
+    for i in range(n // 2 -1, -1, -1):
+        max_heapify(a, i, n)
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # time: O(nlogn)
-        # space: O(k)
-        heap = []
-        for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
+        build_max_heap(nums)
+        i = len(nums) - 1
 
-        return heapq.heappop(heap)
+        for _ in range(k - 1):
+            nums[0], nums[i] = nums[i], nums[0]
+            i -= 1
+            max_heapify(nums, 0, i+1)
+
+        return nums[0]
