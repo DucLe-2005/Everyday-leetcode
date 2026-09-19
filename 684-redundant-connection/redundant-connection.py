45a111d507af5d66b671c3324804c488abce311a
Duc Le <135077375+DucLe-2005@users.noreply.github.com>
2026-09-19 00:04:40 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (28.21%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,35 +1,30 @@
 class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        # time: O(na(n))
-        # space: O(n)
-        
-        parent = list(range(len(edges) + 1))
-        size = [0] * (len(edges) + 1)
+    def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
+        parent = [i for i in range(len(edges) + 1)]
+        size = [1] * (len(edges) + 1)
 
-        def find(x):
+        def find(x: int) -> int:
             if x != parent[x]:
                 parent[x] = find(parent[x])
-            
             return parent[x]
         
-        def union(a, b):
-            root_a = find(a)
-            root_b = find(b)
+        def union(a: int, b: int) -> bool:
+            parent_a = find(a)
+            parent_b = find(b)
 
-            if root_a == root_b:
+            if parent_a == parent_b:
                 return False
             
-            if size[root_a] < size[root_b]:
-                root_a, root_b = root_b, root_a
+            if size[parent_a] < size[parent_b]:
+                parent_a, parent_b = parent_b, parent_a
             
-            parent[root_b] = root_a
-            size[root_a] += size[root_b]
+            parent[parent_b] = parent_a
+            size[parent_a] += size[parent_b]
 
             return True
-
-        redundant = []
+        
         for a, b in edges:
             if not union(a, b):
-                redundant = [a, b]
-        
-        return redundant
+                return [a, b]
+
+            
